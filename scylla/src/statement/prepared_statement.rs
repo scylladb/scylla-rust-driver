ec0acaacf49ac5e0dbc6797d45cb1fb11988c061
Piotr Sarna <sarna@scylladb.com>
2020-10-22 14:46:50 +0200

    transport: allow repreparing prepared statements
    Statements occasionally need to be reprepared, so it's a good idea
    to remember the original text of the query. Also, such a statement
    might need to get its id rewritten, after the session reprepares it.

--- scylla/src/statement/prepared_statement.rs
+++ scylla/src/statement/prepared_statement.rs
@@ -3,14 +3,23 @@ use bytes::Bytes;
 #[derive(Debug)]
 pub struct PreparedStatement {
     id: Bytes,
+    statement: String,
 }
 
 impl PreparedStatement {
-    pub fn new(id: Bytes) -> Self {
-        Self { id }
+    pub fn new(id: Bytes, statement: String) -> Self {
+        Self { id, statement }
     }
 
     pub fn get_id(&self) -> &Bytes {
         &self.id
+    }
+
+    pub fn update_id(&mut self, id: Bytes) {
+        self.id = id
+    }
+
+    pub fn get_statement(&self) -> &str {
+        &self.statement
     }
 }
