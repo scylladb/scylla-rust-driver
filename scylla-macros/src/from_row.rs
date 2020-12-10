080b78dd2eea4ed1cc0bb85d251ef7d36c7aea53
Jasper Visser <jasper.visser@keylane.com>
2020-12-10 23:35:43 +0100

    Removed duplicate code in proc macro

--- scylla-macros/src/from_row.rs
+++ scylla-macros/src/from_row.rs
@@ -1,23 +1,11 @@
 use proc_macro::TokenStream;
 use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields};
+use syn::spanned::Spanned;
 
 /// #[derive(FromRow)] derives FromRow for struct
 /// Works only on simple structs without generics etc
 pub fn from_row_derive(tokens_input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(tokens_input as DeriveInput);
-
-    let struct_name = &input.ident;
-
-    let struct_fields = match &input.data {
-        Data::Struct(data) => {
-            match &data.fields {
-                Fields::Named(named_fields) => named_fields,
-                _ => panic!("derive(FromRow) works only for structs with named fields. Tuples don't need derive."),
-            }
-        },
-        _ => panic!("derive(FromRow) works only on structs!")
-    };
+    let (struct_name, struct_fields) = crate::parser::parse(tokens_input, "FromRow");
 
     // Generates tokens for field_name: field_type::from_cql(vals_iter.next().ok_or(...)?), ...
     let set_fields_code = struct_fields.named.iter().map(|field| {
