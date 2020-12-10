0cce2e10ef8b149b3c96cb6f395e7a101ebe2bf9
Jasper Visser <jasper.visser@keylane.com>
2020-12-10 23:44:54 +0100

    Run cargo fmt

--- scylla-macros/src/parser.rs
+++ scylla-macros/src/parser.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
-use syn::{DeriveInput, parse2, Data, Fields, FieldsNamed};
 use proc_macro2::Ident;
+use syn::{parse2, Data, DeriveInput, Fields, FieldsNamed};
 
 /// Parsers the tokens_input to a DeriveInput and returns the struct name from which it derives and
 /// the named fields
@@ -8,13 +8,14 @@ pub(crate) fn parse(tokens_input: TokenStream, current_derive: &str) -> (Ident, 
     let input = parse2::<DeriveInput>(tokens_input.into()).expect("No DeriveInput");
     let struct_name = input.ident;
     let struct_fields = match input.data {
-        Data::Struct(data) => {
-            match data.fields {
-                Fields::Named(named_fields) => named_fields,
-                _ => panic!("derive({}) works only for structs with named fields. Tuples don't need derive.", current_derive),
-            }
+        Data::Struct(data) => match data.fields {
+            Fields::Named(named_fields) => named_fields,
+            _ => panic!(
+                "derive({}) works only for structs with named fields. Tuples don't need derive.",
+                current_derive
+            ),
         },
-        _ => panic!("derive({}) works only on structs!", current_derive)
+        _ => panic!("derive({}) works only on structs!", current_derive),
     };
 
     (struct_name, struct_fields)
