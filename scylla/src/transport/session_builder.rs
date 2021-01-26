fb373c4fe0acdfbdd69973e505d8699d88d4719c
cvybhu <cvybhu@selade.com>
2021-01-26 20:47:43 +0100

    Add documentation for SessionBuilder

--- scylla/src/transport/session_builder.rs
+++ scylla/src/transport/session_builder.rs
@@ -3,47 +3,157 @@ use super::session::{Session, SessionConfig};
 use super::Compression;
 use std::net::SocketAddr;
 
+/// SessionBuilder is used to create new Session instances
+/// # Example
+///
+/// ```
+/// # use scylla::{Session, SessionBuilder};
+/// # use scylla::transport::Compression;
+/// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+/// let session: Session = SessionBuilder::new()
+///     .known_node("127.0.0.1:9042")
+///     .compression(Some(Compression::Snappy))
+///     .build()
+///     .await?;
+/// # Ok(())
+/// # }
+/// ```
 pub struct SessionBuilder {
     pub config: SessionConfig,
 }
 
 impl SessionBuilder {
+    /// Creates new SessionBuilder with default configuration
+    /// # Default configuration
+    /// * Compression: None
+    ///
     pub fn new() -> Self {
         SessionBuilder {
             config: SessionConfig::new(),
         }
     }
 
+    /// Add a known node with a hostname
+    /// # Examples
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let session: Session = SessionBuilder::new().known_node("127.0.0.1:9042").build().await?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let session: Session = SessionBuilder::new().known_node("db1.example.com").build().await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn known_node(mut self, hostname: impl AsRef<str>) -> Self {
         self.config.add_known_node(hostname);
         self
     }
 
+    /// Add a known node with an IP address
+    /// # Example
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let session: Session = SessionBuilder::new()
+    ///     .known_node_addr(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9042))
+    ///     .build()
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn known_node_addr(mut self, node_addr: SocketAddr) -> Self {
         self.config.add_known_node_addr(node_addr);
         self
     }
 
+    /// Add a list of known nodes with hostnames
+    /// # Example
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let session: Session = SessionBuilder::new()
+    ///     .known_nodes(&["127.0.0.1:9042", "db1.example.com"])
+    ///     .build()
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn known_nodes(mut self, hostnames: &[impl AsRef<str>]) -> Self {
         self.config.add_known_nodes(hostnames);
         self
     }
 
+    /// Add a list of known nodes with IP addresses
+    /// # Example
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let addr1 = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(172, 17, 0, 3)), 9042);
+    /// let addr2 = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(172, 17, 0, 4)), 9042);
+    ///
+    /// let session: Session = SessionBuilder::new()
+    ///     .known_nodes_addr(&[addr1, addr2])
+    ///     .build()
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn known_nodes_addr(mut self, node_addrs: &[SocketAddr]) -> Self {
         self.config.add_known_nodes_addr(node_addrs);
         self
     }
 
+    /// Set preferred Compression algorithm.  
+    /// The default is no compression.  
+    /// If it is not supported by database server Session will fall back to no encryption.  
+    ///
+    /// # Example
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # use scylla::transport::Compression;
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let session: Session = SessionBuilder::new()
+    ///     .known_node("127.0.0.1:9042")
+    ///     .compression(Some(Compression::Snappy))
+    ///     .build()
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn compression(mut self, compression: Option<Compression>) -> Self {
         self.config.compression = compression;
         self
     }
 
+    /// Builds the Session after setting all the options
+    ///
+    /// # Example
+    /// ```
+    /// # use scylla::{Session, SessionBuilder};
+    /// # use scylla::transport::Compression;
+    /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
+    /// let session: Session = SessionBuilder::new()
+    ///     .known_node("127.0.0.1:9042")
+    ///     .compression(Some(Compression::Snappy))
+    ///     .build() // Turns SessionBuilder into Session
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn build(self) -> Result<Session, NewSessionError> {
         Session::connect(self.config).await
     }
 }
 
+/// Creates a [`SessionBuilder`] with default configuration, same as [`SessionBuilder::new`]
 impl Default for SessionBuilder {
     fn default() -> Self {
         SessionBuilder::new()
