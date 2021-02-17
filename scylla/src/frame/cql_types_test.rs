ac0ba0c00f46fc1d9b0d63f9936a0b1f3babfd01
asledz <anita.gaja.sledz@gmail.com>
2021-02-17 22:28:15 +0100

    Add CQL type tests.
    Add CQL type test.
    Additionaly fix clippy warnings in deser_tests in result.
    Applied code review for CQL types.

--- scylla/src/frame/cql_types_test.rs
+++ scylla/src/frame/cql_types_test.rs
@@ -1,19 +1,22 @@
+use crate::transport::session::IntoTypedRows;
+use crate::transport::session::Session;
+use crate::SessionBuilder;
+use std::env;
+
 // TODO: Requires a running local Scylla instance
 #[tokio::test]
 #[ignore]
-async fn test_cql_collections() {
+async fn test_cql_types() {
     // Create connection
-    let uri = std::env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
+    let uri = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
 
     println!("Connecting to {} ...", uri);
 
-    let session = crate::transport::session::Session::connect(uri, None)
-        .await
-        .unwrap();
-    session.refresh_topology().await.unwrap();
+    let session: Session = SessionBuilder::new().known_node(uri).build().await.unwrap();
 
     session.query("CREATE KEYSPACE IF NOT EXISTS ks WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1}", &[]).await.unwrap();
 
+    // Date type test
     session
         .query(
             "CREATE TABLE IF NOT EXISTS ks.days (day date, id int PRIMARY KEY)",
@@ -28,6 +31,14 @@ async fn test_cql_collections() {
         .await
         .unwrap();
 
+    if let Some(rows) = session.query("SELECT day FROM ks.days", &[]).await.unwrap() {
+        for row in rows.into_typed::<(u32,)>() {
+            let day: u32 = row.unwrap().0;
+            println!("day: {}", day);
+        }
+    }
+
+    // Bool test
     session
         .query(
             "CREATE TABLE IF NOT EXISTS ks.truefalse (boolvalue boolean, id int PRIMARY KEY)",
@@ -44,4 +55,45 @@ async fn test_cql_collections() {
         )
         .await
         .unwrap();
+
+    if let Some(rows) = session
+        .query("SELECT boolvalue FROM ks.truefalse", &[])
+        .await
+        .unwrap()
+    {
+        for row in rows.into_typed::<(bool,)>() {
+            let bool_val: bool = row.unwrap().0;
+            println!("bool value: {}", bool_val);
+        }
+    }
+
+    // Float test
+
+    session
+        .query(
+            "CREATE TABLE IF NOT EXISTS ks.floatingpoint (boolvalue float, id int PRIMARY KEY)",
+            &[],
+        )
+        .await
+        .unwrap();
+
+    let val: f32 = 1.42;
+    session
+        .query(
+            "INSERT INTO ks.floatingpoint (float, id) VALUES (?, 1)",
+            (val,),
+        )
+        .await
+        .unwrap();
+
+    if let Some(rows) = session
+        .query("SELECT float FROM ks.floatingpoint", &[])
+        .await
+        .unwrap()
+    {
+        for row in rows.into_typed::<(f32,)>() {
+            let bool_val: f32 = row.unwrap().0;
+            println!("float value: {}", bool_val);
+        }
+    }
 }
--- scylla/src/frame/response/cql_to_rust.rs
+++ scylla/src/frame/response/cql_to_rust.rs
@@ -69,9 +69,9 @@ macro_rules! impl_from_cql_val {
 
 impl_from_cql_val!(i32, as_int); // i32::from_cql<CQLValue>
 impl_from_cql_val!(u32, as_date); // u32::from_cql<CQLValue>
-impl_from_cql_val!(f32, as_float); // i32::from_cql<CQLValue>
-impl_from_cql_val!(f64, as_double); // u32::from_cql<CQLValue>
-impl_from_cql_val!(bool, as_boolean); // u32::from_cql<CQLValue>
+impl_from_cql_val!(f32, as_float); // f32::from_cql<CQLValue>
+impl_from_cql_val!(f64, as_double); // f64::from_cql<CQLValue>
+impl_from_cql_val!(bool, as_boolean); // bool::from_cql<CQLValue>
 impl_from_cql_val!(i64, as_bigint); // i64::from_cql<CQLValue>
 impl_from_cql_val!(String, into_string); // String::from_cql<CQLValue>
 impl_from_cql_val!(IpAddr, as_inet); // IpAddr::from_cql<CQLValue>
