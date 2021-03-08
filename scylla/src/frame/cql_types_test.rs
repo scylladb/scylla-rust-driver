fe2ce2985b128177b0085c385b2fc5a94223c8a4
Michał Sala <0havaker@gmail.com>
2021-03-08 01:49:56 +0100

    Refactor integration tests in cql_types_test.rs
    Split integration tests in test_cql_types function into several
    smaller functions.
    Added more integration tests for Varint and Decimal CQL type.
    Replaced println with asserts for float CQL type.

--- scylla/src/frame/cql_types_test.rs
+++ scylla/src/frame/cql_types_test.rs
@@ -1,282 +1,152 @@
+use crate::cql_to_rust::FromCQLVal;
+use crate::frame::response::result::CQLValue;
+use crate::frame::value::Value;
 use crate::transport::session::IntoTypedRows;
 use crate::transport::session::Session;
 use crate::SessionBuilder;
 use bigdecimal::BigDecimal;
-use num_bigint::{BigInt, ToBigInt};
+use num_bigint::BigInt;
+use std::cmp::PartialEq;
 use std::env;
+use std::fmt::Debug;
 use std::str::FromStr;
 
-// TODO: Requires a running local Scylla instance
-#[tokio::test]
-async fn test_cql_types() {
-    // Create connection
+// Used to prepare a table for test
+// Creates keyspace ks
+// Drops and creates table ks.{table_name} (id int PRIMARY KEY, val {type_name})
+async fn init_test(table_name: &str, type_name: &str) -> Session {
     let uri = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
 
     println!("Connecting to {} ...", uri);
-
     let session: Session = SessionBuilder::new().known_node(uri).build().await.unwrap();
 
-    session.query("CREATE KEYSPACE IF NOT EXISTS ks WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1}", &[]).await.unwrap();
-
-    // Date type test
     session
         .query(
-            "CREATE TABLE IF NOT EXISTS ks.days (day date, id int PRIMARY KEY)",
+            "CREATE KEYSPACE IF NOT EXISTS ks WITH REPLICATION = \
+            {'class' : 'SimpleStrategy', 'replication_factor' : 1}",
             &[],
         )
         .await
         .unwrap();
 
-    let date_ts: u32 = 10;
     session
-        .query("INSERT INTO ks.days (day, id) VALUES (?, 1)", (date_ts,))
+        .query(format!("DROP TABLE IF EXISTS ks.{}", table_name), &[])
         .await
         .unwrap();
 
-    if let Some(rows) = session.query("SELECT day FROM ks.days", &[]).await.unwrap() {
-        for row in rows.into_typed::<(u32,)>() {
-            let day: u32 = row.unwrap().0;
-            println!("day: {}", day);
-        }
-    }
-
-    // Bool test
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS ks.truefalse (boolvalue boolean, id int PRIMARY KEY)",
-            &[],
-        )
-        .await
-        .unwrap();
-
-    let val: bool = true;
-    session
-        .query(
-            "INSERT INTO ks.truefalse (boolvalue, id) VALUES (?, 1)",
-            (val,),
-        )
-        .await
-        .unwrap();
-
-    if let Some(rows) = session
-        .query("SELECT boolvalue FROM ks.truefalse", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(bool,)>() {
-            let bool_val: bool = row.unwrap().0;
-            println!("bool value: {}", bool_val);
-            assert_eq!(bool_val, true);
-        }
-    }
-
-    // Float test
-
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS ks.floatingpoint (floatval float, id int PRIMARY KEY)",
-            &[],
-        )
-        .await
-        .unwrap();
-
-    let val: f32 = 1.42;
-    session
-        .query(
-            "INSERT INTO ks.floatingpoint (floatval, id) VALUES (?, 1)",
-            (val,),
-        )
-        .await
-        .unwrap();
-
-    session
-        .query(
-            "INSERT INTO ks.floatingpoint (floatval, id) VALUES (?, 2)",
-            (f32::MAX,),
-        )
-        .await
-        .unwrap();
-
-    session
-        .query(
-            "INSERT INTO ks.floatingpoint (floatval, id) VALUES (?, 2)",
-            (f32::MIN,),
-        )
-        .await
-        .unwrap();
-
-    if let Some(rows) = session
-        .query("SELECT floatval FROM ks.floatingpoint", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(f32,)>() {
-            let row_val: f32 = row.unwrap().0;
-            println!("float value: {}", row_val);
-        }
-    }
-
-    // Varint
-
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS ks.varint_table (value varint, id int PRIMARY KEY)",
-            &[],
-        )
-        .await
-        .unwrap();
-
-    let val_positive = 10000.to_bigint().unwrap();
-    let val_negative = (-1111111111111111111_i64).to_bigint().unwrap();
-
-    session
-        .query(
-            "INSERT INTO ks.varint_table (value, id) VALUES (?, 1)",
-            (val_positive,),
-        )
-        .await
-        .unwrap();
-
-    session
-        .query(
-            "INSERT INTO ks.varint_table (value, id) VALUES (?, 2)",
-            (val_negative,),
-        )
-        .await
-        .unwrap();
-
-    if let Some(rows) = session
-        .query("SELECT value FROM ks.varint_table where id = 1", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(BigInt,)>() {
-            let bigint_value_row: BigInt = row.unwrap().0;
-            println!("varint value: {}", bigint_value_row);
-            assert_eq!(bigint_value_row, 10000.to_bigint().unwrap());
-        }
-    }
-
-    if let Some(rows) = session
-        .query("SELECT value FROM ks.varint_table where id = 2", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(BigInt,)>() {
-            let bigint_value_row: BigInt = row.unwrap().0;
-            println!("varint value: {}", bigint_value_row);
-            assert_eq!(
-                bigint_value_row,
-                (-1111111111111111111_i64).to_bigint().unwrap()
-            );
-        }
-    }
-
-    // Decimal
-
-    session
-        .query(
-            "CREATE TABLE IF NOT EXISTS ks.decimal_table (value decimal, id int PRIMARY KEY)",
-            &[],
-        )
-        .await
-        .unwrap();
-
-    let scale: i64 = 2;
-    let val_positive = 10000.to_bigint().unwrap();
-    let val_negative = (-1111111111111111111_i64).to_bigint().unwrap();
-
-    let decimal_positive = BigDecimal::from((val_positive, scale));
-    let decimal_negative = BigDecimal::from((val_negative, scale));
-
-    let decimal_other = BigDecimal::from(-234);
-
-    session
-        .query(
-            "INSERT INTO ks.decimal_table (value, id) VALUES (?, 1)",
-            (decimal_positive,),
-        )
-        .await
-        .unwrap();
-
-    if let Some(rows) = session
-        .query("SELECT value FROM ks.decimal_table WHERE id = 1", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(BigDecimal,)>() {
-            let decimal_new: BigDecimal = row.unwrap().0;
-            println!("decimal value: {}", decimal_new);
-            assert_eq!(
-                decimal_new,
-                BigDecimal::from((10000.to_bigint().unwrap(), scale))
-            );
-        }
-    }
-
-    let decimal_str = "-123456789012345678901234567890.123456789999999999";
     session
         .query(
             format!(
-                "INSERT INTO ks.decimal_table (value, id) VALUES ({}, 5)",
-                decimal_str
+                "CREATE TABLE IF NOT EXISTS ks.{} (id int PRIMARY KEY, val {})",
+                table_name, type_name
             ),
-            (),
+            &[],
         )
         .await
         .unwrap();
-
-    if let Some(rows) = session
-        .query("SELECT value FROM ks.decimal_table WHERE id = 5", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(BigDecimal,)>() {
-            let decimal_new: BigDecimal = row.unwrap().0;
-            assert_eq!(decimal_new, BigDecimal::from_str(decimal_str).unwrap());
-        }
-    }
 
     session
-        .query(
-            "INSERT INTO ks.decimal_table (value, id) VALUES (?, 2)",
-            (decimal_negative,),
-        )
-        .await
-        .unwrap();
+}
 
-    if let Some(rows) = session
-        .query("SELECT value FROM ks.decimal_table WHERE id = 2", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(BigDecimal,)>() {
-            let decimal_new: BigDecimal = row.unwrap().0;
-            println!("decimal value: {}", decimal_new);
-            assert_eq!(
-                decimal_new,
-                BigDecimal::from(((-1111111111111111111_i64).to_bigint().unwrap(), scale))
-            );
-        }
+// This function tests serialization and deserialization mechanisms by sending insert and select
+// queries to running Scylla instance.
+// To do so, it:
+// Prepares a table for tests (by creating keyspace ks and table ks.{table_name} using init_test)
+// Runs a test that, for every element of `tests`:
+// - inserts 2 values (one encoded as string and one as bound values) into table ks.{type_name}
+// - selects this 2 values and compares them with expected value
+// Expected values and bound values are computed using T::from_str
+async fn run_tests<T>(tests: &[&str], type_name: &str)
+where
+    T: Value + FromCQLVal<CQLValue> + FromStr + Debug + Clone + PartialEq,
+{
+    let session: Session = init_test(type_name, type_name).await;
+
+    for test in tests.iter() {
+        let insert_string_encoded_value = format!(
+            "INSERT INTO ks.{} (id, val) VALUES (0, {})",
+            type_name, test
+        );
+        session
+            .query(insert_string_encoded_value, &[])
+            .await
+            .unwrap();
+
+        let insert_bound_value = format!("INSERT INTO ks.{} (id, val) VALUES (1, ?)", type_name);
+        let value_to_bound = T::from_str(test).ok().unwrap();
+        session
+            .query(insert_bound_value, (value_to_bound,))
+            .await
+            .unwrap();
+
+        let select_values = format!("SELECT val from ks.{}", type_name);
+        let read_values: Vec<T> = session
+            .query(select_values, &[])
+            .await
+            .unwrap()
+            .unwrap()
+            .into_typed::<(T,)>()
+            .map(Result::unwrap)
+            .map(|row| row.0)
+            .collect::<Vec<_>>();
+
+        let expected_value = T::from_str(test).ok().unwrap();
+        assert_eq!(read_values, vec![expected_value.clone(), expected_value]);
     }
+}
 
-    session
-        .query(
-            "INSERT INTO ks.decimal_table (value, id) VALUES (?, 3)",
-            (decimal_other,),
-        )
-        .await
-        .unwrap();
+#[tokio::test]
+async fn test_varint() {
+    let tests = [
+        "0",
+        "1",
+        "127",
+        "128",
+        "129",
+        "-1",
+        "-128",
+        "-129",
+        "123456789012345678901234567890",
+        "-123456789012345678901234567890",
+    ];
 
-    if let Some(rows) = session
-        .query("SELECT value FROM ks.decimal_table WHERE id = 3", &[])
-        .await
-        .unwrap()
-    {
-        for row in rows.into_typed::<(BigDecimal,)>() {
-            let decimal_new: BigDecimal = row.unwrap().0;
-            println!("decimal value: {}", decimal_new);
-            assert_eq!(decimal_new, BigDecimal::from(-234));
-        }
-    }
+    run_tests::<BigInt>(&tests, "varint").await;
+}
+
+#[tokio::test]
+async fn test_decimal() {
+    let tests = [
+        "4.2",
+        "0",
+        "1.999999999999999999999999999999999999999",
+        "997",
+        "123456789012345678901234567890.1234567890",
+        "-123456789012345678901234567890.1234567890",
+    ];
+
+    run_tests::<BigDecimal>(&tests, "decimal").await;
+}
+
+#[tokio::test]
+async fn test_bool() {
+    let tests = ["true", "false"];
+
+    run_tests::<bool>(&tests, "boolean").await;
+}
+
+#[tokio::test]
+async fn test_float() {
+    let max = f32::MAX.to_string();
+    let min = f32::MIN.to_string();
+    let tests = [
+        "3.14",
+        "997",
+        "0.1",
+        "128",
+        "-128",
+        max.as_str(),
+        min.as_str(),
+    ];
+
+    run_tests::<f32>(&tests, "float").await;
 }
