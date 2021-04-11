6ec1741c832419c86868603800ef06bc90cab6f3
lauranovich <laura@scylladb.com>
2021-04-11 14:57:20 +0300

    new files

---
+++ docs/_utils/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copy contents
+mkdir gh-pages
+cp -r ./docs/_build/dirhtml/. gh-pages
+
+# Create gh-pages branch
+cd gh-pages
+git init
+git config --local user.email "action@scylladb.com"
+git config --local user.name "GitHub Action"
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
+
+# Deploy
+git add .
+git commit -m "Publish docs" || true
+git push origin gh-pages --force
