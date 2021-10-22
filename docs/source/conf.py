e069d991b81fecb64ff5f3246940ffc130d30dd0
David Garcia <hi@davidgarcia.dev>
2021-10-22 11:54:01 +0100

    Fix remove trailing slash in html_baseurl

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -130,7 +130,7 @@ html_sidebars = {'**': ['side-nav.html']}
 htmlhelp_basename = 'ScyllaDocumentationdoc'
 
 # URL which points to the root of the HTML documentation. 
-html_baseurl = 'https://rust-driver.docs.scylladb.com/'
+html_baseurl = 'https://rust-driver.docs.scylladb.com'
 
 # Dictionary of values to pass into the template engine’s context for all pages
 html_context = {'html_baseurl': html_baseurl}
