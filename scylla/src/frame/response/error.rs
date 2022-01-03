scylladb/scylla-rust-driver

commit bff127f0511fac59f214f51b63ef3808a8d89c02

treewide: split consistency to regular and serial
This breaking change splits the Consistency enum into two:
 - Consistency, used for all queries
 - SerialConsistency, an additional flag used only
   for lightweight transactions

The reason for the split is that these values are mutually exclusive:
 - it's only legal to use serial consistency for LWT, and it's not
possible to use it in any other scope. Respectively, it's not possible
to use any of the regular consistency values for defining serial
consistency.

scylla/src/frame/types.rs
@@ -23,9 +23,14 @@ pub enum Consistency {
     All = 0x0005,
     LocalQuorum = 0x0006,
     EachQuorum = 0x0007,
+    LocalOne = 0x000A,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, TryFromPrimitive)]
+#[repr(i16)]
+pub enum SerialConsistency {
     Serial = 0x0008,
     LocalSerial = 0x0009,
-    LocalOne = 0x000A,
 }
 
 impl Default for Consistency {
@@ -401,6 +406,10 @@ pub fn read_consistency(buf: &mut &[u8]) -> Result<Consistency, ParseError> {
 }
 
 pub fn write_consistency(c: Consistency, buf: &mut impl BufMut) {
+    write_short(c as i16, buf);
+}
+
+pub fn write_serial_consistency(c: SerialConsistency, buf: &mut impl BufMut) {
     write_short(c as i16, buf);
 }
 

scylla/src/transport/session.rs
@@ -20,7 +20,7 @@ use crate::frame::value::{BatchValues, SerializedValues, ValueList};
 use crate::prepared_statement::{PartitionKeyError, PreparedStatement};
 use crate::query::Query;
 use crate::routing::{murmur3_token, Token};
-use crate::statement::Consistency;
+use crate::statement::{Consistency, SerialConsistency};
 use crate::tracing::{GetTracingConfig, TracingEvent, TracingInfo};
 use crate::transport::{
     cluster::Cluster,
@@ -1181,7 +1181,7 @@ impl Session {
         let info = Statement::default();
         let config = StatementConfig {
             is_idempotent: true,
-            serial_consistency: Some(Consistency::One),
+            serial_consistency: Some(SerialConsistency::LocalSerial),
             ..Default::default()
         };
 

examples/tracing.rs
@@ -4,7 +4,9 @@
 use anyhow::{anyhow, Result};
 use futures::StreamExt;
 use scylla::batch::Batch;
-use scylla::statement::{prepared_statement::PreparedStatement, query::Query, Consistency};
+use scylla::statement::{
+    prepared_statement::PreparedStatement, query::Query, Consistency, SerialConsistency,
+};
 use scylla::tracing::{GetTracingConfig, TracingInfo};
 use scylla::transport::iterator::RowIterator;
 use scylla::{BatchResult, QueryResult};
@@ -34,6 +36,7 @@ async fn main() -> Result<()> {
     // Create a simple query and enable tracing for it
     let mut query: Query = Query::new("SELECT val from ks.tracing_example".to_string());
     query.set_tracing(true);
+    query.set_serial_consistency(Some(SerialConsistency::LocalSerial));
 
     // QueryResult will contain a tracing_id which can be used to query tracing information
     let query_result: QueryResult = session.query(query.clone(), &[]).await?;

scylla/src/frame/request/batch.rs
@@ -21,7 +21,7 @@ where
     pub statements_count: usize,
     pub batch_type: BatchType,
     pub consistency: types::Consistency,
-    pub serial_consistency: Option<types::Consistency>,
+    pub serial_consistency: Option<types::SerialConsistency>,
     pub timestamp: Option<i64>,
     pub values: Values,
 }
@@ -74,7 +74,7 @@ where
         buf.put_u8(flags);
 
         if let Some(serial_consistency) = self.serial_consistency {
-            types::write_consistency(serial_consistency, buf);
+            types::write_serial_consistency(serial_consistency, buf);
         }
         if let Some(timestamp) = self.timestamp {
             types::write_long(timestamp, buf);

scylla/src/frame/request/query.rs
@@ -34,7 +34,7 @@ impl Request for Query<'_> {
 
 pub struct QueryParameters<'a> {
     pub consistency: types::Consistency,
-    pub serial_consistency: Option<types::Consistency>,
+    pub serial_consistency: Option<types::SerialConsistency>,
     pub timestamp: Option<i64>,
     pub page_size: Option<i32>,
     pub paging_state: Option<Bytes>,
@@ -94,7 +94,7 @@ impl QueryParameters<'_> {
         }
 
         if let Some(serial_consistency) = self.serial_consistency {
-            types::write_consistency(serial_consistency, buf);
+            types::write_serial_consistency(serial_consistency, buf);
         }
 
         if let Some(timestamp) = self.timestamp {

scylla/src/statement/batch.rs
@@ -1,8 +1,8 @@
 use crate::statement::{prepared_statement::PreparedStatement, query::Query};
 use crate::transport::retry_policy::RetryPolicy;
 
-pub use super::Consistency;
 use super::StatementConfig;
+pub use super::{Consistency, SerialConsistency};
 pub use crate::frame::request::batch::BatchType;
 
 /// CQL batch statement.
@@ -56,13 +56,13 @@ impl Batch {
 
     /// Sets the serial consistency to be used when executing this batch.
     /// (Ignored unless the batch is an LWT)
-    pub fn set_serial_consistency(&mut self, sc: Option<Consistency>) {
+    pub fn set_serial_consistency(&mut self, sc: Option<SerialConsistency>) {
         self.config.serial_consistency = sc;
     }
 
     /// Gets the serial consistency to be used when executing this batch.
     /// (Ignored unless the batch is an LWT)
-    pub fn get_serial_consistency(&self) -> Option<Consistency> {
+    pub fn get_serial_consistency(&self) -> Option<SerialConsistency> {
         self.config.serial_consistency
     }
 

scylla/src/statement/mod.rs
@@ -7,11 +7,11 @@ pub mod batch;
 pub mod prepared_statement;
 pub mod query;
 
-pub use crate::frame::types::Consistency;
+pub use crate::frame::types::{Consistency, SerialConsistency};
 
 pub struct StatementConfig {
     pub consistency: Consistency,
-    pub serial_consistency: Option<Consistency>,
+    pub serial_consistency: Option<SerialConsistency>,
 
     pub is_idempotent: bool,
 

scylla/src/statement/prepared_statement.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 
 use super::StatementConfig;
 use crate::frame::response::result::PreparedMetadata;
-use crate::frame::types::Consistency;
+use crate::frame::types::{Consistency, SerialConsistency};
 use crate::frame::value::SerializedValues;
 use crate::transport::retry_policy::RetryPolicy;
 
@@ -142,13 +142,13 @@ impl PreparedStatement {
 
     /// Sets the serial consistency to be used when executing this statement.
     /// (Ignored unless the statement is an LWT)
-    pub fn set_serial_consistency(&mut self, sc: Option<Consistency>) {
+    pub fn set_serial_consistency(&mut self, sc: Option<SerialConsistency>) {
         self.config.serial_consistency = sc;
     }
 
     /// Gets the serial consistency to be used when executing this statement.
     /// (Ignored unless the statement is an LWT)
-    pub fn get_serial_consistency(&self) -> Option<Consistency> {
+    pub fn get_serial_consistency(&self) -> Option<SerialConsistency> {
         self.config.serial_consistency
     }
 

commit 9865a099b70dfc8c0d4666c7262c1fe04b533107

treewide: support parsing serial consistency results
Commit bff127f introduced a regression while splitting consistency
enum into Consistency and SerialConsistency. Namely, our driver
lost the ability to parse SerialConsistency returned as an error
from CQL. The ability is now restored by wrapping the result
in an enum - LegacyConsistency, capable of representing both regular
and serial consistency values.

Fixes #369

scylla/src/frame/response/error.rs
@@ -82,6 +82,7 @@ impl From<Error> for QueryError {
 #[cfg(test)]
 mod tests {
     use super::Error;
+    use crate::frame::types::LegacyConsistency;
     use crate::statement::Consistency;
     use crate::transport::errors::{DbError, WriteType};
     use std::convert::TryInto;
@@ -137,7 +138,7 @@ mod tests {
         assert_eq!(
             error.error,
             DbError::Unavailable {
-                consistency: Consistency::One,
+                consistency: LegacyConsistency::Regular(Consistency::One),
                 required: 2,
                 alive: 3,
             }
@@ -162,7 +163,7 @@ mod tests {
         assert_eq!(
             error.error,
             DbError::WriteTimeout {
-                consistency: Consistency::Quorum,
+                consistency: LegacyConsistency::Regular(Consistency::Quorum),
                 received: -5, // Allow negative values when they don't make sense, it's better than crashing with ProtocolError
                 required: 100,
                 write_type: WriteType::Simple,
@@ -184,7 +185,7 @@ mod tests {
         assert_eq!(
             error.error,
             DbError::ReadTimeout {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 8,
                 required: 32,
                 data_present: false,
@@ -207,7 +208,7 @@ mod tests {
         assert_eq!(
             error.error,
             DbError::ReadFailure {
-                consistency: Consistency::Three,
+                consistency: LegacyConsistency::Regular(Consistency::Three),
                 received: 4,
                 required: 5,
                 numfailures: 6,
@@ -275,7 +276,7 @@ mod tests {
         assert_eq!(
             error.error,
             DbError::WriteFailure {
-                consistency: Consistency::Any,
+                consistency: LegacyConsistency::Regular(Consistency::Any),
                 received: 2,
                 required: 4,
                 numfailures: 8,

scylla/src/frame/types.rs
@@ -33,6 +33,15 @@ pub enum SerialConsistency {
     LocalSerial = 0x0009,
 }
 
+// LegacyConsistency exists, because Scylla may return a SerialConsistency value
+// as Consistency when returning certain error types - the distinction between
+// Consistency and SerialConsistency is not really a thing in CQL.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LegacyConsistency {
+    Regular(Consistency),
+    Serial(SerialConsistency),
+}
+
 impl Default for Consistency {
     fn default() -> Self {
         Consistency::LocalQuorum
@@ -42,6 +51,21 @@ impl Default for Consistency {
 impl std::fmt::Display for Consistency {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+impl std::fmt::Display for SerialConsistency {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::fmt::Display for LegacyConsistency {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Regular(c) => c.fmt(f),
+            Self::Serial(c) => c.fmt(f),
+        }
     }
 }
 
@@ -398,10 +422,16 @@ fn type_uuid() {
     assert_eq!(u, u2);
 }
 
-pub fn read_consistency(buf: &mut &[u8]) -> Result<Consistency, ParseError> {
+pub fn read_consistency(buf: &mut &[u8]) -> Result<LegacyConsistency, ParseError> {
     let raw = read_short(buf)?;
-    let parsed = Consistency::try_from(raw)
-        .map_err(|_| ParseError::BadData(format!("unknown consistency: {}", raw)))?;
+    let parsed = match Consistency::try_from(raw) {
+        Ok(c) => LegacyConsistency::Regular(c),
+        Err(_) => {
+            let parsed_serial = SerialConsistency::try_from(raw)
+                .map_err(|_| ParseError::BadData(format!("unknown consistency: {}", raw)))?;
+            LegacyConsistency::Serial(parsed_serial)
+        }
+    };
     Ok(parsed)
 }
 
@@ -419,7 +449,7 @@ fn type_consistency() {
     let mut buf = Vec::new();
     write_consistency(c, &mut buf);
     let c2 = read_consistency(&mut &*buf).unwrap();
-    assert_eq!(c, c2);
+    assert_eq!(LegacyConsistency::Regular(c), c2);
 
     let c: i16 = 0x1234;
     buf.clear();

scylla/src/transport/errors.rs
@@ -1,8 +1,8 @@
 //! This module contains various erros which can be returned by [`Session`](crate::Session)
 
 use crate::frame::frame_errors::{FrameError, ParseError};
+use crate::frame::types::LegacyConsistency;
 use crate::frame::value::SerializeValuesError;
-use crate::statement::Consistency;
 use std::io::ErrorKind;
 use std::sync::Arc;
 use thiserror::Error;
@@ -92,7 +92,7 @@ pub enum DbError {
     )]
     Unavailable {
         /// Consistency level of the query
-        consistency: Consistency,
+        consistency: LegacyConsistency,
         /// Number of nodes required to be alive to satisfy required consistency level
         required: i32,
         /// Found number of active nodes
@@ -116,7 +116,7 @@ pub enum DbError {
             (consistency: {consistency}, received: {received}, required: {required}, data_present: {data_present})")]
     ReadTimeout {
         /// Consistency level of the query
-        consistency: Consistency,
+        consistency: LegacyConsistency,
         /// Number of nodes that responded to the read request
         received: i32,
         /// Number of nodes required to respond to satisfy required consistency level
@@ -130,7 +130,7 @@ pub enum DbError {
             (consistency: {consistency}, received: {received}, required: {required}, write_type: {write_type})")]
     WriteTimeout {
         /// Consistency level of the query
-        consistency: Consistency,
+        consistency: LegacyConsistency,
         /// Number of nodes that responded to the write request
         received: i32,
         /// Number of nodes required to respond to satisfy required consistency level
@@ -147,7 +147,7 @@ pub enum DbError {
     )]
     ReadFailure {
         /// Consistency level of the query
-        consistency: Consistency,
+        consistency: LegacyConsistency,
         /// Number of nodes that responded to the read request
         received: i32,
         /// Number of nodes required to respond to satisfy required consistency level
@@ -166,7 +166,7 @@ pub enum DbError {
     )]
     WriteFailure {
         /// Consistency level of the query
-        consistency: Consistency,
+        consistency: LegacyConsistency,
         /// Number of ndoes that responded to the read request
         received: i32,
         /// Number of nodes required to respond to satisfy required consistency level
@@ -385,6 +385,7 @@ impl From<&str> for WriteType {
 #[cfg(test)]
 mod tests {
     use super::{DbError, QueryError, WriteType};
+    use crate::frame::types::LegacyConsistency;
     use crate::statement::Consistency;
 
     #[test]
@@ -416,7 +417,7 @@ mod tests {
     fn dberror_full_info() {
         // Test that DbError::Unavailable is displayed correctly
         let db_error = DbError::Unavailable {
-            consistency: Consistency::Three,
+            consistency: LegacyConsistency::Regular(Consistency::Three),
             required: 3,
             alive: 2,
         };

scylla/src/transport/iterator.rs
@@ -15,6 +15,7 @@ use tokio::sync::mpsc;
 use super::errors::QueryError;
 use crate::cql_to_rust::{FromRow, FromRowError};
 
+use crate::frame::types::LegacyConsistency;
 use crate::frame::{
     response::{
         result,
@@ -297,7 +298,7 @@ where
                 let query_info = QueryInfo {
                     error: &last_error,
                     is_idempotent: self.query_is_idempotent,
-                    consistency: self.query_consistency,
+                    consistency: LegacyConsistency::Regular(self.query_consistency),
                 };
 
                 match self.retry_session.decide_should_retry(query_info) {

scylla/src/transport/retry_policy.rs
@@ -2,7 +2,7 @@
 //! To decide when to retry a query the `Session` can use any object which implements
 //! the `RetryPolicy` trait
 
-use crate::statement::Consistency;
+use crate::frame::types::LegacyConsistency;
 use crate::transport::errors::{DbError, QueryError, WriteType};
 
 /// Information about a failed query
@@ -14,7 +14,7 @@ pub struct QueryInfo<'a> {
     /// If set to `false` it is unknown whether it is idempotent
     pub is_idempotent: bool,
     /// Consistency with which the query failed
-    pub consistency: Consistency,
+    pub consistency: LegacyConsistency,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -211,6 +211,7 @@ impl RetrySession for DefaultRetrySession {
 #[cfg(test)]
 mod tests {
     use super::{DefaultRetryPolicy, QueryInfo, RetryDecision, RetryPolicy};
+    use crate::frame::types::LegacyConsistency;
     use crate::statement::Consistency;
     use crate::transport::errors::{BadQuery, DbError, QueryError, WriteType};
     use std::io::ErrorKind;
@@ -220,7 +221,7 @@ mod tests {
         QueryInfo {
             error,
             is_idempotent,
-            consistency: Consistency::One,
+            consistency: LegacyConsistency::Regular(Consistency::One),
         }
     }
 
@@ -257,14 +258,14 @@ mod tests {
             DbError::Unauthorized,
             DbError::ConfigError,
             DbError::ReadFailure {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 2,
                 required: 1,
                 numfailures: 1,
                 data_present: false,
             },
             DbError::WriteFailure {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 1,
                 required: 2,
                 numfailures: 1,
@@ -335,7 +336,7 @@ mod tests {
     fn default_unavailable() {
         let error = QueryError::DbError(
             DbError::Unavailable {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 required: 2,
                 alive: 1,
             },
@@ -369,7 +370,7 @@ mod tests {
         // Enough responses and data_present == true
         let enough_responses_with_data = QueryError::DbError(
             DbError::ReadTimeout {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 2,
                 required: 2,
                 data_present: true,
@@ -402,7 +403,7 @@ mod tests {
         // Enough responses but data_present == false
         let enough_responses_no_data = QueryError::DbError(
             DbError::ReadTimeout {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 2,
                 required: 2,
                 data_present: false,
@@ -427,7 +428,7 @@ mod tests {
         // Not enough responses, data_present == true
         let not_enough_responses_with_data = QueryError::DbError(
             DbError::ReadTimeout {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 1,
                 required: 2,
                 data_present: true,
@@ -456,7 +457,7 @@ mod tests {
         // WriteType == BatchLog
         let good_write_type = QueryError::DbError(
             DbError::WriteTimeout {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 1,
                 required: 2,
                 write_type: WriteType::BatchLog,
@@ -485,7 +486,7 @@ mod tests {
         // WriteType != BatchLog
         let bad_write_type = QueryError::DbError(
             DbError::WriteTimeout {
-                consistency: Consistency::Two,
+                consistency: LegacyConsistency::Regular(Consistency::Two),
                 received: 4,
                 required: 2,
                 write_type: WriteType::Simple,

scylla/src/transport/session.rs
@@ -1,6 +1,7 @@
 //! `Session` is the main object used in the driver.\
 //! It manages all connections to the cluster and allows to perform queries.
 
+use crate::frame::types::LegacyConsistency;
 use bytes::Bytes;
 use futures::future::join_all;
 use futures::future::try_join_all;
@@ -1133,7 +1134,9 @@ impl Session {
                 let query_info = QueryInfo {
                     error: last_error.as_ref().unwrap(),
                     is_idempotent,
-                    consistency: consistency.unwrap_or(self.default_consistency),
+                    consistency: LegacyConsistency::Regular(
+                        consistency.unwrap_or(self.default_consistency),
+                    ),
                 };
 
                 match retry_session.decide_should_retry(query_info) {