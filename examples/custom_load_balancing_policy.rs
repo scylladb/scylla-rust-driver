7298f5570781e22cd79d4479f21bf32d5f3f1a62
Michał Sala <michal.sala@scylladb.com>
2022-01-25 13:35:00 +0100

    examples: custom load balancing
    Created a simple example that shows how to make and use a custom load
    balancing policy.

---
+++ examples/custom_load_balancing_policy.rs
@@ -0,0 +1,51 @@
+use anyhow::Result;
+use scylla::{
+    load_balancing::{LoadBalancingPolicy, Statement},
+    transport::{ClusterData, Node},
+    Session, SessionBuilder,
+};
+use std::{env, sync::Arc};
+
+/// Example load balancing policy that prefers nodes from favorite datacenter
+struct CustomLoadBalancingPolicy {
+    fav_datacenter_name: String,
+}
+
+impl LoadBalancingPolicy for CustomLoadBalancingPolicy {
+    fn plan<'a>(
+        &self,
+        _statement: &Statement,
+        cluster: &'a ClusterData,
+    ) -> Box<dyn Iterator<Item = Arc<Node>> + Send + Sync + 'a> {
+        let fav_dc_info = cluster
+            .get_datacenters_info()
+            .get(&self.fav_datacenter_name);
+
+        match fav_dc_info {
+            Some(info) => Box::new(info.nodes.iter().cloned()),
+            // If there is no dc with provided name, fallback to other datacenters
+            None => Box::new(cluster.get_nodes_info().iter().cloned()),
+        }
+    }
+
+    fn name(&self) -> String {
+        "CustomPolicy".to_string()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let uri = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
+
+    let custom_load_balancing = CustomLoadBalancingPolicy {
+        fav_datacenter_name: "PL".to_string(),
+    };
+
+    let _session: Session = SessionBuilder::new()
+        .known_node(uri)
+        .load_balancing(Arc::new(custom_load_balancing))
+        .build()
+        .await?;
+
+    Ok(())
+}
