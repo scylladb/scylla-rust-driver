f857846a06d1529b3b8c69434dc4c779af005cdc
Piotr Sarna <sarna@scylladb.com>
2022-06-10 16:43:22 +0200

    examples: extend custom_load_balancing_policy example
    The extended example now also covers setting up per-statement
    load balancing policy, which takes precedence over the one
    specified in the session.

--- examples/custom_load_balancing_policy.rs
+++ examples/custom_load_balancing_policy.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use scylla::{
-    load_balancing::{LoadBalancingPolicy, Statement},
+    load_balancing::{LoadBalancingPolicy, RoundRobinPolicy, Statement},
+    query::Query,
     transport::{ClusterData, Node},
     Session, SessionBuilder,
 };
 use std::{env, sync::Arc};
+use tracing::info;
 
 /// Example load balancing policy that prefers nodes from favorite datacenter
 struct CustomLoadBalancingPolicy {
@@ -17,6 +19,7 @@ impl LoadBalancingPolicy for CustomLoadBalancingPolicy {
         _statement: &Statement,
         cluster: &'a ClusterData,
     ) -> Box<dyn Iterator<Item = Arc<Node>> + Send + Sync + 'a> {
+        info!("Yay, picking my favorite DC again!");
         let fav_dc_info = cluster
             .get_datacenters_info()
             .get(&self.fav_datacenter_name);
@@ -35,17 +38,45 @@ impl LoadBalancingPolicy for CustomLoadBalancingPolicy {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
     let uri = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
 
-    let custom_load_balancing = CustomLoadBalancingPolicy {
+    let custom_load_balancing = Arc::new(CustomLoadBalancingPolicy {
         fav_datacenter_name: "PL".to_string(),
-    };
+    });
 
-    let _session: Session = SessionBuilder::new()
+    let session: Session = SessionBuilder::new()
         .known_node(uri)
-        .load_balancing(Arc::new(custom_load_balancing))
+        .load_balancing(custom_load_balancing.clone())
         .build()
         .await?;
+
+    session
+        .query("SELECT host_id FROM system.local", &[])
+        .await
+        .unwrap();
+
+    let mut config = scylla::statement::StatementConfig {
+        load_balancing_policy: Some(Arc::new(RoundRobinPolicy::new())),
+        ..Default::default()
+    };
+
+    session
+        .query(
+            Query::with_config("SELECT host_id FROM system.local", config.clone()),
+            &[],
+        )
+        .await
+        .unwrap();
+
+    config.load_balancing_policy = Some(custom_load_balancing);
+    session
+        .query(
+            Query::with_config("SELECT host_id FROM system.local", config),
+            &[],
+        )
+        .await
+        .unwrap();
 
     Ok(())
 }
