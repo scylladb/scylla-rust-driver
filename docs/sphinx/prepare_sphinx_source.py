394b5d8422a529be2bca6286dff02e5f67ad1b00
Jan Ciolek <jan.ciolek@scylladb.com>
2022-08-19 19:59:26 +0200

    Remove #s

--- docs/sphinx/prepare_sphinx_source.py
+++ docs/sphinx/prepare_sphinx_source.py
@@ -2,8 +2,44 @@ import os
 import shutil
 import pathlib
 
+
+"""
+In mdbook blocks of rust code can contain hidden lines starting with #
+So for example when there is:
+
+```rust
+let a: i32 = 0;
+# let hidden = "not visible in html"
+```
+
+Only the first line would be visible in the generated html.
+Sphinx does handle this and just puts these lines in the html,
+so to avoid this they are removed using this function.
+"""
+def remove_hidden_code_lines(md_file_text):
+    text_split = md_file_text.split("```")
+
+    result = []
+
+    for i in range(0, len(text_split)):
+        cur_chunk = text_split[i]
+
+        if i % 2 == 0 or not cur_chunk.startswith("rust"):
+            result.append(cur_chunk)
+            continue
+
+        new_chunk_lines = []
+        chunk_lines = cur_chunk.split('\n')
+        for line in cur_chunk.split('\n'):
+            if not line.lstrip().startswith("#"):
+                new_chunk_lines.append(line)
+        new_chunk = "\n".join(new_chunk_lines)
+        result.append(new_chunk)
+
+    return "```".join(result)
+
 def prepare_sphinx_md_file(original_md_file):
-    return original_md_file
+    return remove_hidden_code_lines(original_md_file)
 
 original_source_dir = "../source"
 output_dir = "source"
