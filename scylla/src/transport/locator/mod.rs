8af89312b86154d95c1a80861263d8c0b77c202b
Michał Sala <michal.sala@scylladb.com>
2022-12-01 15:53:58 +0100

    transport: locator: add Replicas struct
    `Replicas` is a contiainer holding replicas, enabling unified use of
    both borrowed and owned `Node` sequences. The motivation for its
    introduction is that it will be used later to hold:
    - References to precomputed replica slices
    - Owned replica sequences computed on the fly

--- scylla/src/transport/locator/mod.rs
+++ scylla/src/transport/locator/mod.rs
@@ -1,3 +1,4 @@
+mod replicas;
 mod token_ring;
 
 pub use token_ring::TokenRing;
---
+++ scylla/src/transport/locator/replicas.rs
@@ -0,0 +1,62 @@
+use itertools::Either;
+
+use crate::transport::{Node, NodeRef};
+use std::{ops::Index, sync::Arc};
+
+/// Container holding replicas, enabling unified use of both borrowed and owned `Node` sequences.
+///
+/// This type is very similar to `Cow`, but unlike `Cow`,
+/// it holds references in an `Owned` variant `Vec`.
+#[derive(Debug)]
+pub(crate) enum ReplicasArray<'a> {
+    Borrowed(&'a [Arc<Node>]),
+    Owned(Vec<NodeRef<'a>>),
+}
+
+impl<'a> ReplicasArray<'a> {
+    pub(crate) fn get(&self, index: usize) -> Option<NodeRef<'a>> {
+        match self {
+            ReplicasArray::Borrowed(slice) => slice.get(index),
+            ReplicasArray::Owned(vec) => vec.get(index).copied(),
+        }
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        match self {
+            ReplicasArray::Borrowed(slice) => slice.len(),
+            ReplicasArray::Owned(vec) => vec.len(),
+        }
+    }
+
+    pub(crate) fn iter<'s>(&'s self) -> impl Iterator<Item = NodeRef<'a>> + 's {
+        match self {
+            ReplicasArray::Borrowed(slice) => Either::Left(slice.iter()),
+            ReplicasArray::Owned(vec) => Either::Right(vec.iter().copied()),
+        }
+    }
+}
+
+impl<'a> FromIterator<NodeRef<'a>> for ReplicasArray<'a> {
+    fn from_iter<T: IntoIterator<Item = NodeRef<'a>>>(iter: T) -> Self {
+        Self::Owned(iter.into_iter().collect())
+    }
+}
+
+impl<'a> From<&'a [Arc<Node>]> for ReplicasArray<'a> {
+    fn from(item: &'a [Arc<Node>]) -> Self {
+        Self::Borrowed(item)
+    }
+}
+
+impl<'a> Index<usize> for ReplicasArray<'a> {
+    type Output = Arc<Node>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match self {
+            ReplicasArray::Borrowed(b) => &b[index],
+            ReplicasArray::Owned(o) => o[index],
+        }
+    }
+}
+
+pub(crate) const EMPTY_REPLICAS: ReplicasArray<'static> = ReplicasArray::Borrowed(&[]);
