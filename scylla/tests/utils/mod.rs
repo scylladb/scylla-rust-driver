5f768de829bd162cbe8caee32f429fbb80f3f8cd
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2023-01-05 14:03:10 +0100

    proxy: Autogenerate exclusive IPs for proxies
    This is done using a single atomic. No longer do developers have to look
    up all addresses taken by older tests, but it is done automatically.

--- scylla/tests/utils/mod.rs
+++ scylla/tests/utils/mod.rs
@@ -40,7 +40,6 @@ impl LoadBalancingPolicy for FixedOrderLoadBalancer {
 
 pub async fn test_with_3_node_cluster<F, Fut>(
     shard_awareness: ShardAwareness,
-    first_proxy_node_addr_last_octet: u16,
     test: F,
 ) -> Result<(), ProxyError>
 where
@@ -49,11 +48,11 @@ where
 {
     init_logger();
     let real1_uri = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
-    let proxy1_uri = format!("127.0.0.{}:9042", first_proxy_node_addr_last_octet);
+    let proxy1_uri = format!("{}:9042", scylla_proxy::get_exclusive_local_address());
     let real2_uri = env::var("SCYLLA_URI2").unwrap_or_else(|_| "127.0.0.2:9042".to_string());
-    let proxy2_uri = format!("127.0.0.{}:9042", first_proxy_node_addr_last_octet + 1);
+    let proxy2_uri = format!("{}:9042", scylla_proxy::get_exclusive_local_address());
     let real3_uri = env::var("SCYLLA_URI3").unwrap_or_else(|_| "127.0.0.3:9042".to_string());
-    let proxy3_uri = format!("127.0.0.{}:9042", first_proxy_node_addr_last_octet + 2);
+    let proxy3_uri = format!("{}:9042", scylla_proxy::get_exclusive_local_address());
 
     let real1_addr = SocketAddr::from_str(real1_uri.as_str()).unwrap();
     let proxy1_addr = SocketAddr::from_str(proxy1_uri.as_str()).unwrap();
