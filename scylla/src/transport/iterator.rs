331b5905c80bc2d603c8ea5c4a59adecce19ff7c
Piotr Dulikowski <piodul@scylladb.com>
2023-03-13 16:19:52 +0100

    iterator: introduce ready_some_ok! macro
    The iterator module has a small number of functions which return
    Poll<Option<Result<T, E>>>, call other functions that return such a
    stacked type and are support to continue if value obtained is not of
    form Ready(Some(Ok(x))). This requires a multiline pattern match
    instruction, which is basically boilerplate.
    
    This commit introduces the ready_some_ok! macro which hides the
    aforementioned boilerplate. Conceptually, it is similar to the existing
    std::task::ready! macro.
    
    Co-authored-by: Wojciech Przytuła <wojciech.przytula@scylladb.com>

--- scylla/src/transport/iterator.rs
+++ scylla/src/transport/iterator.rs
@@ -38,6 +38,20 @@ use crate::transport::NodeRef;
 use tracing::{trace, trace_span, warn, Instrument};
 use uuid::Uuid;
 
+// Like std::task::ready!, but handles the whole stack of Poll<Option<Result<>>>.
+// If it matches Poll::Ready(Some(Ok(_))), then it returns the innermost value,
+// otherwise it returns from the surrounding function.
+macro_rules! ready_some_ok {
+    ($e:expr) => {
+        match $e {
+            Poll::Ready(Some(Ok(x))) => x,
+            Poll::Ready(Some(Err(err))) => return Poll::Ready(Some(Err(err.into()))),
+            Poll::Ready(None) => return Poll::Ready(None),
+            Poll::Pending => return Poll::Pending,
+        }
+    };
+}
+
 /// Iterator over rows returned by paged queries\
 /// Allows to easily access rows without worrying about handling multiple pages
 pub struct LegacyRowIterator {
@@ -78,28 +92,22 @@ impl LegacyRowIterator {
         let mut s = self.as_mut();
 
         if s.is_current_page_exhausted() {
-            match Pin::new(&mut s.page_receiver).poll_recv(cx) {
-                Poll::Ready(Some(Ok(received_page))) => {
-                    let rows = match received_page
-                        .rows
-                        // As RowIteratorWorker manages paging itself, the paging state response
-                        // returned to the user is always NoMorePages. It used to be so before
-                        // the deserialization refactor, too.
-                        .into_legacy_rows(PagingStateResponse::NoMorePages)
-                    {
-                        Ok(rows) => rows,
-                        Err(err) => return Poll::Ready(Some(Err(err.into()))),
-                    };
-                    s.current_page = rows;
-                    s.current_row_idx = 0;
+            let received_page = ready_some_ok!(Pin::new(&mut s.page_receiver).poll_recv(cx));
+            let rows = match received_page
+                .rows
+                // As RowIteratorWorker manages paging itself, the paging state response
+                // returned to the user is always NoMorePages. It used to be so before
+                // the deserialization refactor, too.
+                .into_legacy_rows(PagingStateResponse::NoMorePages)
+            {
+                Ok(rows) => rows,
+                Err(err) => return Poll::Ready(Some(Err(err.into()))),
+            };
+            s.current_page = rows;
+            s.current_row_idx = 0;
 
-                    if let Some(tracing_id) = received_page.tracing_id {
-                        s.tracing_ids.push(tracing_id);
-                    }
-                }
-                Poll::Ready(Some(Err(err))) => return Poll::Ready(Some(Err(err))),
-                Poll::Ready(None) => return Poll::Ready(None),
-                Poll::Pending => return Poll::Pending,
+            if let Some(tracing_id) = received_page.tracing_id {
+                s.tracing_ids.push(tracing_id);
             }
         }
 
@@ -937,19 +945,9 @@ impl<RowT: FromRow> Stream for LegacyTypedRowIterator<RowT> {
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut s = self.as_mut();
 
-        let next_elem: Option<Result<Row, QueryError>> =
-            match Pin::new(&mut s.row_iterator).poll_next(cx) {
-                Poll::Ready(next_elem) => next_elem,
-                Poll::Pending => return Poll::Pending,
-            };
-
-        let next_ready: Option<Self::Item> = match next_elem {
-            Some(Ok(next_row)) => Some(RowT::from_row(next_row).map_err(|e| e.into())),
-            Some(Err(e)) => Some(Err(e.into())),
-            None => None,
-        };
-
-        Poll::Ready(next_ready)
+        let next_row = ready_some_ok!(Pin::new(&mut s.row_iterator).poll_next(cx));
+        let typed_row_res = RowT::from_row(next_row).map_err(|e| e.into());
+        Poll::Ready(Some(typed_row_res))
     }
 }
 
