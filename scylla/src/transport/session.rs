009ced9f06a9f222607385ae4526ac7be0feb031
Piotr Dulikowski <piodul@scylladb.com>
2023-03-14 15:08:27 +0100

    iterator: adjust to the new deserialization framework
    This commit finishes the work related to adjusting the iterators module
    to the new deserialization framework.
    
    The equivalent of the old RowIterator is now RawIterator (notice the
    vowel change). Despite the name, it cannot actually be iterated on, as
    it does not have any information about the column types. After calling
    the `into_typed()` method it gets converted into TypedRowIterator that
    can actually be iterated on.
    
    Unfortunately, due to the limitations of the Stream trait, currently the
    TypedRowIterator cannot deserialize types that need to borrow from the
    serialized frame contents.
    
    Co-authored-by: Wojciech Przytuła <wojciech.przytula@scylladb.com>

--- scylla/src/transport/session.rs
+++ scylla/src/transport/session.rs
@@ -4,6 +4,7 @@
 use crate::batch::batch_values;
 #[cfg(feature = "cloud")]
 use crate::cloud::CloudConfig;
+use crate::LegacyQueryResult;
 
 use crate::history;
 use crate::history::HistoryListener;
@@ -42,7 +43,8 @@ use super::connection::QueryResponse;
 use super::connection::SslConfig;
 use super::errors::TracingProtocolError;
 use super::execution_profile::{ExecutionProfile, ExecutionProfileHandle, ExecutionProfileInner};
-use super::legacy_query_result::{LegacyQueryResult, MaybeFirstRowTypedError};
+use super::iterator::RawIterator;
+use super::legacy_query_result::MaybeFirstRowTypedError;
 #[cfg(feature = "cloud")]
 use super::node::CloudEndpoint;
 use super::node::KnownNode;
@@ -910,20 +912,21 @@ impl Session {
             .access();
 
         if values.is_empty() {
-            LegacyRowIterator::new_for_query(
+            RawIterator::new_for_query(
                 query,
                 execution_profile,
                 self.cluster.get_data(),
                 self.metrics.clone(),
             )
             .await
+            .map(RawIterator::into_legacy)
         } else {
-            // Making LegacyRowIterator::new_for_query work with values is too hard (if even possible)
+            // Making RawIterator::new_for_query work with values is too hard (if even possible)
             // so instead of sending one prepare to a specific connection on each iterator query,
             // we fully prepare a statement beforehand.
             let prepared = self.prepare(query).await?;
             let values = prepared.serialize_values(&values)?;
-            LegacyRowIterator::new_for_prepared_statement(PreparedIteratorConfig {
+            RawIterator::new_for_prepared_statement(PreparedIteratorConfig {
                 prepared,
                 values,
                 execution_profile,
@@ -931,6 +934,7 @@ impl Session {
                 metrics: self.metrics.clone(),
             })
             .await
+            .map(RawIterator::into_legacy)
         }
     }
 
@@ -1313,7 +1317,7 @@ impl Session {
             .unwrap_or_else(|| self.get_default_execution_profile_handle())
             .access();
 
-        LegacyRowIterator::new_for_prepared_statement(PreparedIteratorConfig {
+        RawIterator::new_for_prepared_statement(PreparedIteratorConfig {
             prepared,
             values: serialized_values,
             execution_profile,
@@ -1321,6 +1325,7 @@ impl Session {
             metrics: self.metrics.clone(),
         })
         .await
+        .map(RawIterator::into_legacy)
     }
 
     /// Perform a batch request.\
