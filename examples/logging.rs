3b598d69509e10b6d2b06d3d274b86f7291362a5
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2023-04-26 09:29:34 +0200

    examples: logging: removed obsolete comment
    The executed request no longer produces any warnings.

--- examples/logging.rs
+++ examples/logging.rs
@@ -19,7 +19,6 @@ async fn main() -> Result<()> {
     let session: Session = SessionBuilder::new().known_node(uri).build().await?;
     session.query("CREATE KEYSPACE IF NOT EXISTS ks WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1}", &[]).await?;
 
-    // This query should generate a warning message
     session.query("USE ks", &[]).await?;
 
     Ok(())
