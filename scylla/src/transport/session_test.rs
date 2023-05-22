ab71c7ad9bec09bd80c5e717cffc7ed73c406ef0
Jan Ciolek <jan.ciolek@scylladb.com>
2023-05-22 22:05:27 +0200

    session_test: add a SimpleStrategy test
    Scylla is moving away from SimpleStrategy,
    in general users should use NetworkTopologyStrategy everywhere now.
    
    Still it's good to test that the driver works with a SimpleStrategy keyspace,
    it's still a case that is supported, even if it's slowly getting deprecated.
    
    This test runs a few queries in a SimpleStrategy keyspace.
    
    Signed-off-by: Jan Ciolek <jan.ciolek@scylladb.com>

--- scylla/src/transport/session_test.rs
+++ scylla/src/transport/session_test.rs
@@ -2731,3 +2731,70 @@ async fn test_get_keyspace_name() {
         .unwrap();
     assert_eq!(*session.get_keyspace().unwrap(), ks);
 }
+
+/// It's recommended to use NetworkTopologyStrategy everywhere, so most tests use only NetworkTopologyStrategy.
+/// We still support SimpleStrategy, so to make sure that SimpleStrategy works correctly this test runs
+/// a few queries in a SimpleStrategy keyspace.
+#[tokio::test]
+async fn simple_strategy_test() {
+    let ks = unique_keyspace_name();
+    let session = create_new_session_builder().build().await.unwrap();
+
+    session
+        .query(
+            format!(
+                "CREATE KEYSPACE {} WITH REPLICATION = \
+                {{'class': 'SimpleStrategy', 'replication_factor': 1}}",
+                ks
+            ),
+            (),
+        )
+        .await
+        .unwrap();
+
+    session
+        .query(
+            format!(
+                "CREATE TABLE {}.tab (p int, c int, r int, PRIMARY KEY (p, c, r))",
+                ks
+            ),
+            (),
+        )
+        .await
+        .unwrap();
+
+    session
+        .query(
+            format!("INSERT INTO {}.tab (p, c, r) VALUES (1, 2, 3)", ks),
+            (),
+        )
+        .await
+        .unwrap();
+
+    session
+        .query(
+            format!("INSERT INTO {}.tab (p, c, r) VALUES (?, ?, ?)", ks),
+            (4, 5, 6),
+        )
+        .await
+        .unwrap();
+
+    let prepared = session
+        .prepare(format!("INSERT INTO {}.tab (p, c, r) VALUES (?, ?, ?)", ks))
+        .await
+        .unwrap();
+
+    session.execute(&prepared, (7, 8, 9)).await.unwrap();
+
+    let mut rows: Vec<(i32, i32, i32)> = session
+        .query(format!("SELECT p, c, r FROM {}.tab", ks), ())
+        .await
+        .unwrap()
+        .rows_typed::<(i32, i32, i32)>()
+        .unwrap()
+        .map(|r| r.unwrap())
+        .collect::<Vec<(i32, i32, i32)>>();
+    rows.sort();
+
+    assert_eq!(rows, vec![(1, 2, 3), (4, 5, 6), (7, 8, 9)]);
+}
