e61387809fa3fab9e340ffd0c8a92ac8c78eb8d1
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2023-08-31 16:33:13 +0200

    re-export value in cql mod

--- scylla/src/cql/mod.rs
+++ scylla/src/cql/mod.rs
@@ -1,2 +1,11 @@
 //! This module re-exports entities from scylla-cql crate in a structured,
 //! controlled way, so that only appropriate items are exposed for users.
+
+pub mod value {
+    pub use scylla_cql::frame::value::{
+        Counter, CqlDuration, Date, MaybeUnset, SerializeValuesError, SerializedValues, Time,
+        Timestamp, Unset, Value, ValueList,
+    };
+}
+
+pub use value::{Value, ValueList};
