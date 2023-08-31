fd56931134e71f9c640453296d408bcf28508a3c
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2023-08-31 10:45:06 +0200

    extract tests module from transport tests

--- scylla/src/transport/mod.rs
+++ scylla/src/transport/mod.rs
@@ -2,13 +2,3 @@ pub mod query_result;
 
 pub use crate::frame::{Authenticator, Compression};
 pub use scylla_cql::errors;
-
-#[cfg(test)]
-mod authenticate_test;
-#[cfg(test)]
-mod cql_collections_test;
-
-#[cfg(test)]
-mod cql_types_test;
-#[cfg(test)]
-mod cql_value_test;
