e6d6d3e5bd97ec746e12640a3b861e3cbdffd404
Piotr Dulikowski <piodul@scylladb.com>
2023-10-06 15:11:00 +0200

    docs: update conf.py after 0.10.0 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,14 +14,14 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # Build documentation for the following tags and branches
-TAGS = ['v0.9.0']
+TAGS = ['v0.9.0', 'v0.10.0']
 BRANCHES = ['main']
 # Set the latest version.
-LATEST_VERSION = 'v0.9.0'
+LATEST_VERSION = 'v0.10.0'
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = ['main']
 # Set which versions are deprecated
-DEPRECATED_VERSIONS = []
+DEPRECATED_VERSIONS = ['v0.9.0']
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
