579069838b55a6d7d4a98aa0f1a9f4775d78c192
Piotr Dulikowski <piodul@scylladb.com>
2023-12-04 15:54:13 +0100

    types/serialize: add a shorthand for constructing SerializationError

--- scylla-cql/src/types/serialize/mod.rs
+++ scylla-cql/src/types/serialize/mod.rs
@@ -13,6 +13,14 @@ pub use writers::{
 #[derive(Debug, Clone, Error)]
 pub struct SerializationError(Arc<dyn Error + Send + Sync>);
 
+impl SerializationError {
+    /// Constructs a new `SerializationError`.
+    #[inline]
+    pub fn new(err: impl Error + Send + Sync + 'static) -> SerializationError {
+        SerializationError(Arc::new(err))
+    }
+}
+
 impl Display for SerializationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "SerializationError: {}", self.0)
