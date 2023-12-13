cb7787ddf14fb05500d2ea0f9776ca1508860170
Piotr Dulikowski <piodul@scylladb.com>
2023-12-13 23:44:39 +0100

    raw_batch: implement RawBatchValueAdapter
    Implement an adapter layer which takes a `BatchValues` object (which
    needs type information to serialize), pairs it with an iterator over
    `RowSerializationContext` objects and returns something which implements
    `RawBatchValues` (which don't require type information to serialize).
    
    It will be used by the `scylla` crate to pass batch data to `scylla-cql`
    in a type-erased form.

--- scylla-cql/src/types/serialize/raw_batch.rs
+++ scylla-cql/src/types/serialize/raw_batch.rs
@@ -1,15 +1,16 @@
 //! Contains the [`RawBatchValues`] and [`RawBatchValuesIterator`] trait and their
 //! implementations.
 
-use super::row::SerializedValues;
+use super::batch::{BatchValues, BatchValuesIterator};
+use super::row::{RowSerializationContext, SerializedValues};
 use super::{RowWriter, SerializationError};
 
 /// Represents a list of sets of values for a batch statement.
 ///
-/// Unlike [`BatchValues`](super::batch::BatchValues), it doesn't require type
+/// Unlike [`BatchValues`]), it doesn't require type
 /// information from the statements of the batch in order to be serialized.
 ///
-/// This is a lower level trait than [`BatchValues`](super::batch::BatchValues)
+/// This is a lower level trait than [`BatchValues`])
 /// and is only used for interaction between the code in `scylla` and
 /// `scylla-cql` crates. If you are a regular user of the driver, you shouldn't
 /// care about this trait at all.
@@ -33,7 +34,7 @@ pub trait RawBatchValues {
 /// items being iterated over, instead it allows calling methods of the underlying
 /// [`SerializeRow`](super::row::SerializeRow) trait while advancing the iterator.
 ///
-/// Unlike [`BatchValuesIterator`](super::batch::BatchValuesIterator), it doesn't
+/// Unlike [`BatchValuesIterator`], it doesn't
 /// need type information for serialization.
 pub trait RawBatchValuesIterator<'a> {
     /// Serializes the next set of values in the sequence and advances the iterator.
@@ -59,7 +60,7 @@ pub trait RawBatchValuesIterator<'a> {
     }
 }
 
-/// An implementation used by `scylla-proxy`
+// An implementation used by `scylla-proxy`
 impl RawBatchValues for Vec<SerializedValues> {
     type RawBatchValuesIter<'r> = std::slice::Iter<'r, SerializedValues>
     where
@@ -91,5 +92,71 @@ impl<'r> RawBatchValuesIterator<'r> for std::slice::Iter<'r, SerializedValues> {
     #[inline]
     fn count(self) -> usize {
         <_ as Iterator>::count(self)
+    }
+}
+
+/// Takes `BatchValues` and an iterator over contexts, and turns them into a `RawBatchValues`.
+pub struct RawBatchValuesAdapter<BV, CTX> {
+    batch_values: BV,
+    contexts: CTX,
+}
+
+impl<BV, CTX> RawBatchValuesAdapter<BV, CTX> {
+    /// Creates a new `RawBatchValuesAdapter` object.
+    #[inline]
+    pub fn new(batch_values: BV, contexts: CTX) -> Self {
+        Self {
+            batch_values,
+            contexts,
+        }
+    }
+}
+
+impl<'ctx, BV, CTX> RawBatchValues for RawBatchValuesAdapter<BV, CTX>
+where
+    BV: BatchValues,
+    CTX: Iterator<Item = RowSerializationContext<'ctx>> + Clone,
+{
+    type RawBatchValuesIter<'r> = RawBatchValuesIteratorAdapter<BV::BatchValuesIter<'r>, CTX>
+    where
+        Self: 'r;
+
+    #[inline]
+    fn batch_values_iter(&self) -> Self::RawBatchValuesIter<'_> {
+        RawBatchValuesIteratorAdapter {
+            batch_values_iterator: self.batch_values.batch_values_iter(),
+            contexts: self.contexts.clone(),
+        }
+    }
+}
+
+/// Takes `BatchValuesIterator` and an iterator over contexts, and turns them into a `RawBatchValuesIterator`.
+pub struct RawBatchValuesIteratorAdapter<BVI, CTX> {
+    batch_values_iterator: BVI,
+    contexts: CTX,
+}
+
+impl<'bvi, 'ctx, BVI, CTX> RawBatchValuesIterator<'bvi> for RawBatchValuesIteratorAdapter<BVI, CTX>
+where
+    BVI: BatchValuesIterator<'bvi>,
+    CTX: Iterator<Item = RowSerializationContext<'ctx>>,
+{
+    #[inline]
+    fn serialize_next(&mut self, writer: &mut RowWriter) -> Option<Result<(), SerializationError>> {
+        let ctx = self.contexts.next()?;
+        self.batch_values_iterator.serialize_next(&ctx, writer)
+    }
+
+    fn is_empty_next(&mut self) -> Option<bool> {
+        self.contexts.next()?;
+        let ret = self.batch_values_iterator.is_empty_next()?;
+        Some(ret)
+    }
+
+    #[inline]
+    fn skip_next(&mut self) -> Option<()> {
+        self.contexts.next()?;
+        self.batch_values_iterator.skip_next()?;
+        Some(())
     }
 }
