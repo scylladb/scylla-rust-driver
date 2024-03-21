a2a83db7d0efd408e0ad76b7ffed75a3d0b9827a
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2024-03-21 17:25:21 +0100

    deser/value: set up test machinery
    The machinery is going to be used for testing proper deserialization of
    all supported types. The tests will involve compatibility checks with
    the previous framework.
    
    Co-authored-by: Piotr Dulikowski <piodul@scylladb.com>

--- scylla-cql/src/types/deserialize/value.rs
+++ scylla-cql/src/types/deserialize/value.rs
@@ -5,6 +5,7 @@ use std::fmt::Display;
 use thiserror::Error;
 
 use super::{DeserializationError, FrameSlice, TypeCheckError};
+use crate::frame::frame_errors::ParseError;
 use crate::frame::response::result::ColumnType;
 
 /// A type that can be deserialized from a column value inside a row that was
@@ -132,10 +133,102 @@ fn mk_deser_err_named(
 /// Describes why deserialization of some of the built-in types failed.
 #[derive(Debug)]
 #[non_exhaustive]
-pub enum BuiltinDeserializationErrorKind {}
+pub enum BuiltinDeserializationErrorKind {
+    /// A generic deserialization failure - legacy error type.
+    GenericParseError(ParseError),
+}
 
 impl Display for BuiltinDeserializationErrorKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Ok(())
+        match self {
+            BuiltinDeserializationErrorKind::GenericParseError(err) => err.fmt(f),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bytes::{BufMut, Bytes, BytesMut};
+
+    use std::fmt::Debug;
+
+    use crate::frame::response::cql_to_rust::FromCqlVal;
+    use crate::frame::response::result::{deser_cql_value, ColumnType, CqlValue};
+    use crate::frame::types;
+    use crate::types::deserialize::{DeserializationError, FrameSlice};
+    use crate::types::serialize::value::SerializeValue;
+    use crate::types::serialize::CellWriter;
+
+    use super::{mk_deser_err, BuiltinDeserializationErrorKind, DeserializeValue};
+
+    // Checks that both new and old serialization framework
+    // produces the same results in this case
+    fn compat_check<T>(typ: &ColumnType, raw: Bytes)
+    where
+        T: for<'f> DeserializeValue<'f>,
+        T: FromCqlVal<Option<CqlValue>>,
+        T: Debug + PartialEq,
+    {
+        let mut slice = raw.as_ref();
+        let mut cell = types::read_bytes_opt(&mut slice).unwrap();
+        let old = T::from_cql(
+            cell.as_mut()
+                .map(|c| deser_cql_value(typ, c))
+                .transpose()
+                .unwrap(),
+        )
+        .unwrap();
+        let new = deserialize::<T>(typ, &raw).unwrap();
+        assert_eq!(old, new);
+    }
+
+    fn compat_check_serialized<T>(typ: &ColumnType, val: &dyn SerializeValue)
+    where
+        T: for<'f> DeserializeValue<'f>,
+        T: FromCqlVal<Option<CqlValue>>,
+        T: Debug + PartialEq,
+    {
+        let raw = serialize(typ, val);
+        compat_check::<T>(typ, raw);
+    }
+
+    fn deserialize<'frame, T>(
+        typ: &'frame ColumnType,
+        bytes: &'frame Bytes,
+    ) -> Result<T, DeserializationError>
+    where
+        T: DeserializeValue<'frame>,
+    {
+        <T as DeserializeValue<'frame>>::type_check(typ)
+            .map_err(|typecheck_err| DeserializationError(typecheck_err.0))?;
+        let mut frame_slice = FrameSlice::new(bytes);
+        let value = frame_slice.read_cql_bytes().map_err(|err| {
+            mk_deser_err::<T>(typ, BuiltinDeserializationErrorKind::GenericParseError(err))
+        })?;
+        <T as DeserializeValue<'frame>>::deserialize(typ, value)
+    }
+
+    fn make_bytes(cell: &[u8]) -> Bytes {
+        let mut b = BytesMut::new();
+        append_bytes(&mut b, cell);
+        b.freeze()
+    }
+
+    fn serialize(typ: &ColumnType, value: &dyn SerializeValue) -> Bytes {
+        let mut bytes = Bytes::new();
+        serialize_to_buf(typ, value, &mut bytes);
+        bytes
+    }
+
+    fn serialize_to_buf(typ: &ColumnType, value: &dyn SerializeValue, buf: &mut Bytes) {
+        let mut v = Vec::new();
+        let writer = CellWriter::new(&mut v);
+        value.serialize(typ, writer).unwrap();
+        *buf = v.into();
+    }
+
+    fn append_bytes(b: &mut impl BufMut, cell: &[u8]) {
+        b.put_i32(cell.len() as i32);
+        b.put_slice(cell);
     }
 }
