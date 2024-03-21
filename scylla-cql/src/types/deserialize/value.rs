538ae47fe559fc6b901645e8b45f54ed77f89fe6
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2024-03-21 14:46:29 +0100

    value: impl DeserializeCql for fixed numeric types
    Co-authored-by: Piotr Dulikowski <piodul@scylladb.com>

--- scylla-cql/src/types/deserialize/value.rs
+++ scylla-cql/src/types/deserialize/value.rs
@@ -66,6 +66,75 @@ where
     }
 }
 
+macro_rules! impl_strict_type {
+    ($cql_name:literal, $t:ty, $cql_type:pat, $conv:expr $(, $l:lifetime)?) => {
+        impl<$($l,)? 'frame> DeserializeCql<'frame> for $t
+        where
+            $('frame: $l)?
+        {
+            fn type_check(typ: &ColumnType) -> Result<(), ParseError> {
+                // TODO: Format the CQL type names in the same notation
+                // that ScyllaDB/Casssandra uses internally and include them
+                // in such form in the error message
+                match typ {
+                    $cql_type => Ok(()),
+                    _ => Err(ParseError::BadIncomingData(format!(
+                        "Expected {}, got {:?}",
+                        $cql_name, typ,
+                    ))),
+                }
+            }
+
+            fn deserialize(
+                typ: &'frame ColumnType,
+                v: Option<FrameSlice<'frame>>,
+            ) -> Result<Self, ParseError> {
+                $conv(typ, v)
+            }
+        }
+    };
+}
+
+// fixed numeric types
+
+macro_rules! impl_fixed_numeric_type {
+    ($cql_name:literal, $t:ty, $col_type:pat) => {
+        impl_strict_type!(
+            $cql_name,
+            $t,
+            $col_type,
+            |_typ: &'frame ColumnType, v: Option<FrameSlice<'frame>>| {
+                const SIZE: usize = std::mem::size_of::<$t>();
+                let val = ensure_not_null(v)?;
+                let arr = ensure_exact_length::<SIZE>($cql_name, val)?;
+                Ok(<$t>::from_be_bytes(arr))
+            }
+        );
+    };
+}
+
+impl_strict_type!(
+    "boolean",
+    bool,
+    ColumnType::Boolean,
+    |_typ: &'frame ColumnType, v: Option<FrameSlice<'frame>>| {
+        let val = ensure_not_null(v)?;
+        let arr = ensure_exact_length::<1>("boolean", val)?;
+        Ok(arr[0] != 0x00)
+    }
+);
+
+impl_fixed_numeric_type!("tinyint", i8, ColumnType::TinyInt);
+impl_fixed_numeric_type!("smallint", i16, ColumnType::SmallInt);
+impl_fixed_numeric_type!("int", i32, ColumnType::Int);
+impl_fixed_numeric_type!(
+    "bigint or counter",
+    i64,
+    ColumnType::BigInt | ColumnType::Counter
+);
+impl_fixed_numeric_type!("float", f32, ColumnType::Float);
+impl_fixed_numeric_type!("double", f64, ColumnType::Double);
+
 // Utilities
 
 fn ensure_not_null(v: Option<FrameSlice>) -> Result<&[u8], ParseError> {
@@ -75,6 +144,20 @@ fn ensure_not_null(v: Option<FrameSlice>) -> Result<&[u8], ParseError> {
             "Expected a non-null value".to_string(),
         )),
     }
+}
+
+fn ensure_exact_length<const SIZE: usize>(
+    cql_name: &str,
+    v: &[u8],
+) -> Result<[u8; SIZE], ParseError> {
+    v.try_into().map_err(|_| {
+        ParseError::BadIncomingData(format!(
+            "The type {} requires {} bytes, but got {}",
+            cql_name,
+            SIZE,
+            v.len(),
+        ))
+    })
 }
 
 #[cfg(test)]
@@ -92,6 +175,74 @@ mod tests {
     use crate::types::serialize::CellWriter;
 
     use super::DeserializeCql;
+
+    #[test]
+    fn test_integral() {
+        let tinyint = make_bytes(&[0x01]);
+        let decoded_tinyint = deserialize::<i8>(&ColumnType::TinyInt, &tinyint).unwrap();
+        assert_eq!(decoded_tinyint, 0x01);
+
+        let smallint = make_bytes(&[0x01, 0x02]);
+        let decoded_smallint = deserialize::<i16>(&ColumnType::SmallInt, &smallint).unwrap();
+        assert_eq!(decoded_smallint, 0x0102);
+
+        let int = make_bytes(&[0x01, 0x02, 0x03, 0x04]);
+        let decoded_int = deserialize::<i32>(&ColumnType::Int, &int).unwrap();
+        assert_eq!(decoded_int, 0x01020304);
+
+        let bigint = make_bytes(&[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+        let decoded_bigint = deserialize::<i64>(&ColumnType::BigInt, &bigint).unwrap();
+        assert_eq!(decoded_bigint, 0x0102030405060708);
+    }
+
+    #[test]
+    fn test_floating_point() {
+        let float = make_bytes(&[63, 0, 0, 0]);
+        let decoded_float = deserialize::<f32>(&ColumnType::Float, &float).unwrap();
+        assert_eq!(decoded_float, 0.5);
+
+        let double = make_bytes(&[64, 0, 0, 0, 0, 0, 0, 0]);
+        let decoded_double = deserialize::<f64>(&ColumnType::Double, &double).unwrap();
+        assert_eq!(decoded_double, 2.0);
+    }
+
+    #[test]
+    fn test_from_cql_value_compatibility() {
+        // This test should have a sub-case for each type
+        // that implements FromCqlValue
+
+        // fixed size integers
+        for i in 0..7 {
+            let v: i8 = 1 << i;
+            compat_check::<i8>(&ColumnType::TinyInt, make_bytes(&v.to_be_bytes()));
+            compat_check::<i8>(&ColumnType::TinyInt, make_bytes(&(-v).to_be_bytes()));
+        }
+        for i in 0..15 {
+            let v: i16 = 1 << i;
+            compat_check::<i16>(&ColumnType::SmallInt, make_bytes(&v.to_be_bytes()));
+            compat_check::<i16>(&ColumnType::SmallInt, make_bytes(&(-v).to_be_bytes()));
+        }
+        for i in 0..31 {
+            let v: i32 = 1 << i;
+            compat_check::<i32>(&ColumnType::Int, make_bytes(&v.to_be_bytes()));
+            compat_check::<i32>(&ColumnType::Int, make_bytes(&(-v).to_be_bytes()));
+        }
+        for i in 0..63 {
+            let v: i64 = 1 << i;
+            compat_check::<i64>(&ColumnType::BigInt, make_bytes(&v.to_be_bytes()));
+            compat_check::<i64>(&ColumnType::BigInt, make_bytes(&(-v).to_be_bytes()));
+        }
+
+        // bool
+        compat_check::<bool>(&ColumnType::Boolean, make_bytes(&[0]));
+        compat_check::<bool>(&ColumnType::Boolean, make_bytes(&[1]));
+
+        // fixed size floating point types
+        compat_check::<f32>(&ColumnType::Float, make_bytes(&123f32.to_be_bytes()));
+        compat_check::<f32>(&ColumnType::Float, make_bytes(&(-123f32).to_be_bytes()));
+        compat_check::<f64>(&ColumnType::Double, make_bytes(&123f64.to_be_bytes()));
+        compat_check::<f64>(&ColumnType::Double, make_bytes(&(-123f64).to_be_bytes()));
+    }
 
     // Checks that both new and old serialization framework
     // produces the same results in this case
