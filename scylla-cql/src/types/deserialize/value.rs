b9465a5d453ae3e5a099c834fad949d84d294d24
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2024-03-21 14:46:29 +0100

    value: impl DeserializeValue for fixed numeric types
    Co-authored-by: Piotr Dulikowski <piodul@scylladb.com>

--- scylla-cql/src/types/deserialize/value.rs
+++ scylla-cql/src/types/deserialize/value.rs
@@ -117,6 +117,88 @@ where
     }
 }
 
+macro_rules! impl_strict_type {
+    ($t:ty, [$($cql:ident)|+], $conv:expr $(, $l:lifetime)?) => {
+        impl<$($l,)? 'frame> DeserializeValue<'frame> for $t
+        where
+            $('frame: $l)?
+        {
+            fn type_check(typ: &ColumnType) -> Result<(), TypeCheckError> {
+                // TODO: Format the CQL type names in the same notation
+                // that ScyllaDB/Cassandra uses internally and include them
+                // in such form in the error message
+                exact_type_check!(typ, $($cql),*);
+                Ok(())
+            }
+
+            fn deserialize(
+                typ: &'frame ColumnType,
+                v: Option<FrameSlice<'frame>>,
+            ) -> Result<Self, DeserializationError> {
+                $conv(typ, v)
+            }
+        }
+    };
+
+    // Convenience pattern for omitting brackets if type-checking as single types.
+    ($t:ty, $cql:ident, $conv:expr $(, $l:lifetime)?) => {
+        impl_strict_type!($t, [$cql], $conv $(, $l)*);
+    };
+}
+
+macro_rules! impl_emptiable_strict_type {
+    ($t:ty, [$($cql:ident)|+], $conv:expr $(, $l:lifetime)?) => {
+        impl<$($l,)?> Emptiable for $t {}
+
+        impl_strict_type!($t, [$($cql)|*], $conv $(, $l)*);
+    };
+
+    // Convenience pattern for omitting brackets if type-checking as single types.
+    ($t:ty, $cql:ident, $conv:expr $(, $l:lifetime)?) => {
+        impl_emptiable_strict_type!($t, [$cql], $conv $(, $l)*);
+    };
+
+}
+
+// fixed numeric types
+
+macro_rules! impl_fixed_numeric_type {
+    ($t:ty, [$($cql:ident)|+]) => {
+        impl_emptiable_strict_type!(
+            $t,
+            [$($cql)|*],
+            |typ: &'frame ColumnType, v: Option<FrameSlice<'frame>>| {
+                const SIZE: usize = std::mem::size_of::<$t>();
+                let val = ensure_not_null_slice::<Self>(typ, v)?;
+                let arr = ensure_exact_length::<Self, SIZE>(typ, val)?;
+                Ok(<$t>::from_be_bytes(*arr))
+            }
+        );
+    };
+
+    // Convenience pattern for omitting brackets if type-checking as single types.
+    ($t:ty, $cql:ident) => {
+        impl_fixed_numeric_type!($t, [$cql]);
+    };
+}
+
+impl_emptiable_strict_type!(
+    bool,
+    Boolean,
+    |typ: &'frame ColumnType, v: Option<FrameSlice<'frame>>| {
+        let val = ensure_not_null_slice::<Self>(typ, v)?;
+        let arr = ensure_exact_length::<Self, 1>(typ, val)?;
+        Ok(arr[0] != 0x00)
+    }
+);
+
+impl_fixed_numeric_type!(i8, TinyInt);
+impl_fixed_numeric_type!(i16, SmallInt);
+impl_fixed_numeric_type!(i32, Int);
+impl_fixed_numeric_type!(i64, [BigInt | Counter]);
+impl_fixed_numeric_type!(f32, Float);
+impl_fixed_numeric_type!(f64, Double);
+
 // Utilities
 
 fn ensure_not_null_frame_slice<'frame, T>(
@@ -131,6 +213,21 @@ fn ensure_not_null_slice<'frame, T>(
     v: Option<FrameSlice<'frame>>,
 ) -> Result<&'frame [u8], DeserializationError> {
     ensure_not_null_frame_slice::<T>(typ, v).map(|frame_slice| frame_slice.as_slice())
+}
+
+fn ensure_exact_length<'frame, T, const SIZE: usize>(
+    typ: &ColumnType,
+    v: &'frame [u8],
+) -> Result<&'frame [u8; SIZE], DeserializationError> {
+    v.try_into().map_err(|_| {
+        mk_deser_err::<T>(
+            typ,
+            BuiltinDeserializationErrorKind::ByteLengthMismatch {
+                expected: SIZE,
+                got: v.len(),
+            },
+        )
+    })
 }
 
 // Error facilities
@@ -186,11 +283,21 @@ use exact_type_check;
 /// Describes why type checking some of the built-in types failed.
 #[derive(Debug, Clone)]
 #[non_exhaustive]
-pub enum BuiltinTypeCheckErrorKind {}
+pub enum BuiltinTypeCheckErrorKind {
+    /// Expected one from a list of particular types.
+    MismatchedType {
+        /// The list of types that the Rust type can deserialize from.
+        expected: &'static [ColumnType],
+    },
+}
 
 impl Display for BuiltinTypeCheckErrorKind {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Ok(())
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BuiltinTypeCheckErrorKind::MismatchedType { expected } => {
+                write!(f, "expected one of the CQL types: {expected:?}")
+            }
+        }
     }
 }
 
@@ -236,6 +343,9 @@ pub enum BuiltinDeserializationErrorKind {
 
     /// Expected non-null value, got null.
     ExpectedNonNull,
+
+    /// The length of read value in bytes is different than expected for the Rust type.
+    ByteLengthMismatch { expected: usize, got: usize },
 }
 
 impl Display for BuiltinDeserializationErrorKind {
@@ -245,6 +355,11 @@ impl Display for BuiltinDeserializationErrorKind {
             BuiltinDeserializationErrorKind::ExpectedNonNull => {
                 f.write_str("expected a non-null value, got null")
             }
+            BuiltinDeserializationErrorKind::ByteLengthMismatch { expected, got } => write!(
+                f,
+                "the CQL type requires {} bytes, but got {}",
+                expected, got,
+            ),
         }
     }
 }
@@ -263,6 +378,83 @@ mod tests {
     use crate::types::serialize::CellWriter;
 
     use super::{mk_deser_err, BuiltinDeserializationErrorKind, DeserializeValue};
+
+    #[test]
+    fn test_integral() {
+        let tinyint = make_bytes(&[0x01]);
+        let decoded_tinyint = deserialize::<i8>(&ColumnType::TinyInt, &tinyint).unwrap();
+        assert_eq!(decoded_tinyint, 0x01);
+
+        let smallint = make_bytes(&[0x01, 0x02]);
+        let decoded_smallint = deserialize::<i16>(&ColumnType::SmallInt, &smallint).unwrap();
+        assert_eq!(decoded_smallint, 0x0102);
+
+        let int = make_bytes(&[0x01, 0x02, 0x03, 0x04]);
+        let decoded_int = deserialize::<i32>(&ColumnType::Int, &int).unwrap();
+        assert_eq!(decoded_int, 0x01020304);
+
+        let bigint = make_bytes(&[0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+        let decoded_bigint = deserialize::<i64>(&ColumnType::BigInt, &bigint).unwrap();
+        assert_eq!(decoded_bigint, 0x0102030405060708);
+    }
+
+    #[test]
+    fn test_bool() {
+        for boolean in [true, false] {
+            let boolean_bytes = make_bytes(&[boolean as u8]);
+            let decoded_bool = deserialize::<bool>(&ColumnType::Boolean, &boolean_bytes).unwrap();
+            assert_eq!(decoded_bool, boolean);
+        }
+    }
+
+    #[test]
+    fn test_floating_point() {
+        let float = make_bytes(&[63, 0, 0, 0]);
+        let decoded_float = deserialize::<f32>(&ColumnType::Float, &float).unwrap();
+        assert_eq!(decoded_float, 0.5);
+
+        let double = make_bytes(&[64, 0, 0, 0, 0, 0, 0, 0]);
+        let decoded_double = deserialize::<f64>(&ColumnType::Double, &double).unwrap();
+        assert_eq!(decoded_double, 2.0);
+    }
+
+    #[test]
+    fn test_from_cql_value_compatibility() {
+        // This test should have a sub-case for each type
+        // that implements FromCqlValue
+
+        // fixed size integers
+        for i in 0..7 {
+            let v: i8 = 1 << i;
+            compat_check::<i8>(&ColumnType::TinyInt, make_bytes(&v.to_be_bytes()));
+            compat_check::<i8>(&ColumnType::TinyInt, make_bytes(&(-v).to_be_bytes()));
+        }
+        for i in 0..15 {
+            let v: i16 = 1 << i;
+            compat_check::<i16>(&ColumnType::SmallInt, make_bytes(&v.to_be_bytes()));
+            compat_check::<i16>(&ColumnType::SmallInt, make_bytes(&(-v).to_be_bytes()));
+        }
+        for i in 0..31 {
+            let v: i32 = 1 << i;
+            compat_check::<i32>(&ColumnType::Int, make_bytes(&v.to_be_bytes()));
+            compat_check::<i32>(&ColumnType::Int, make_bytes(&(-v).to_be_bytes()));
+        }
+        for i in 0..63 {
+            let v: i64 = 1 << i;
+            compat_check::<i64>(&ColumnType::BigInt, make_bytes(&v.to_be_bytes()));
+            compat_check::<i64>(&ColumnType::BigInt, make_bytes(&(-v).to_be_bytes()));
+        }
+
+        // bool
+        compat_check::<bool>(&ColumnType::Boolean, make_bytes(&[0]));
+        compat_check::<bool>(&ColumnType::Boolean, make_bytes(&[1]));
+
+        // fixed size floating point types
+        compat_check::<f32>(&ColumnType::Float, make_bytes(&123f32.to_be_bytes()));
+        compat_check::<f32>(&ColumnType::Float, make_bytes(&(-123f32).to_be_bytes()));
+        compat_check::<f64>(&ColumnType::Double, make_bytes(&123f64.to_be_bytes()));
+        compat_check::<f64>(&ColumnType::Double, make_bytes(&(-123f64).to_be_bytes()));
+    }
 
     // Checks that both new and old serialization framework
     // produces the same results in this case
