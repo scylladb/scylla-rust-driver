3455abf814d097cc3bfe3f666650b92b4b6bbaf5
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2024-05-10 07:30:09 +0200

    treewide: silence mock structs unused warning
    With a new version of Rust toolchain, our never constructed structs used
    for testing macros began to trigger rustc warnings. They are silenced
    explicitly using #[allow(unused)].

--- scylla/tests/integration/hygiene.rs
+++ scylla/tests/integration/hygiene.rs
@@ -64,6 +64,7 @@ macro_rules! test_crate {
             assert_eq!(sv, sv2);
         }
 
+        #[allow(unused)]
         #[derive(_scylla::macros::SerializeCql, _scylla::macros::SerializeRow)]
         #[scylla(crate = _scylla)]
         struct TestStructNew {
