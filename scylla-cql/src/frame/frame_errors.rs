73c16eea8a56e9a759ef414eb15471703672d6bf
muzarski <mikolaj.uzarski@scylladb.com>
2024-07-01 10:03:24 +0200

    f_errors: CqlErrorParseError

--- scylla-cql/src/frame/frame_errors.rs
+++ scylla-cql/src/frame/frame_errors.rs
@@ -36,6 +36,8 @@ pub enum FrameError {
 #[derive(Error, Debug)]
 pub enum ParseError {
     #[error(transparent)]
+    CqlErrorParseError(#[from] CqlErrorParseError),
+    #[error(transparent)]
     CqlAuthChallengeParseError(#[from] CqlAuthChallengeParseError),
     #[error(transparent)]
     CqlAuthSuccessParseError(#[from] CqlAuthSuccessParseError),
@@ -63,6 +65,22 @@ pub enum ParseError {
     SerializationError(#[from] SerializationError),
     #[error(transparent)]
     CqlTypeError(#[from] CqlTypeError),
+}
+
+/// An error type returned when deserialization of ERROR response fails.
+#[non_exhaustive]
+#[derive(Error, Debug)]
+pub enum CqlErrorParseError {
+    #[error("Malformed error code: {0}")]
+    ErrorCodeParseError(LowLevelDeserializationError),
+    #[error("Malformed error reason: {0}")]
+    ReasonParseError(LowLevelDeserializationError),
+    #[error("Malformed error field {field} of DB error {db_error}: {err}")]
+    MalformedErrorField {
+        db_error: &'static str,
+        field: &'static str,
+        err: LowLevelDeserializationError,
+    },
 }
 
 /// An error type returned when deserialization of AUTH_CHALLENGE response fails.
