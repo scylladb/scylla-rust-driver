78286c3f7cbe5067d16c70597b4c87c582e0c5d9
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2024-08-22 17:32:43 +0200

    Update docs for 0.13.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Global variables
 
 # Build documentation for the following tags and branches
-TAGS = ['v0.12.0', 'v0.13.1']
+TAGS = ['v0.12.0', 'v0.13.2']
 BRANCHES = ['main']
 # Set the latest version.
-LATEST_VERSION = 'v0.13.1'
+LATEST_VERSION = 'v0.13.2'
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = ['main']
 # Set which versions are deprecated
