3624aece2a8ecea56bd5825e50bc019564701896
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2024-12-05 16:25:02 +0100

    codewide: deprecate legacy deserialization API's unmarked remnants
    There were some remnants from the legacy deserialization API that had
    not been marked with #[deprecated] attribute.

--- scylla/src/transport/speculative_execution.rs
+++ scylla/src/transport/speculative_execution.rs
@@ -110,6 +110,7 @@ fn can_be_ignored<ResT>(result: &Result<ResT, QueryError>) -> bool {
             QueryError::EmptyPlan => false,
 
             // Errors that should not appear here, thus should not be ignored
+            #[allow(deprecated)]
             QueryError::NextRowError(_)
             | QueryError::IntoLegacyQueryResultError(_)
             | QueryError::TimeoutError
