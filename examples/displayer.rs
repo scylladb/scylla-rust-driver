dac202216d6276a3325c3279b34e2d1e3a92e30a
michal <michaln1596@gmail.com>
2024-12-07 14:43:33 +0100

    added Wrappers for timeuuid, duration, date, inet

--- examples/displayer.rs
+++ examples/displayer.rs
@@ -1,12 +1,8 @@
 use anyhow::Result;
-use futures::StreamExt as _;
-use futures::TryStreamExt as _;
-use scylla::frame::response::result::Row;
 use scylla::transport::session::Session;
 use scylla::DeserializeRow;
 use scylla::QueryRowsResult;
 use scylla::SessionBuilder;
-use tracing_subscriber::field::display;
 use std::env;
 
 #[tokio::main]
@@ -53,14 +49,6 @@ async fn main() -> Result<()> {
         .execute_unpaged(&prepared, (44_i32, "I'm prepared 3!"))
         .await?;
 
-    // Rows can be parsed as tuples
-    let mut iter = session
-        .query_iter("SELECT a, b, c FROM examples_ks.basic", &[])
-        .await?
-        .rows_stream::<(i32, i32, String)>()?;
-    while let Some((a, b, c)) = iter.try_next().await? {
-        println!("a, b, c: {}, {}, {}", a, b, c);
-    }
 
     // Or as custom structs that derive DeserializeRow
     #[allow(unused)]
@@ -80,5 +68,100 @@ async fn main() -> Result<()> {
     println!("DISPLAYER:");
     println!("{}", displayer);
 
+
+    // example 2
+    session
+    .query_unpaged(
+        "CREATE TABLE IF NOT EXISTS examples_ks.basic4 (a int, b int, c text, d int, timest timestamp, bytes blob, fl float, db double, time1 time, primary key (a, c))",
+        &[],
+    )
+    .await?;
+
+    println!("Created table examples_ks.basic2");
+    session
+    .query_unpaged(
+        "INSERT INTO examples_ks.basic4 
+        (a, b, c, d, timest, bytes, fl, db, time1) 
+        VALUES 
+        (1, 10, 'example text', 3, toTimestamp(now()), textAsBlob('sample bytes'), 3.14, 2.718281, '14:30:00');",
+        &[],
+    )
+    .await?;
+    println!("insert1 done");
+
+    println!("SELECT * FROM examples_ks.basic2");
+
+    let result2 : QueryRowsResult= session
+        .query_unpaged("SELECT * FROM examples_ks.basic4", &[])
+        .await?
+        .into_rows_result()?;
+   
+    let displayer = result2.rows_displayer();
+    println!("DISPLAYER:");
+    println!("{}", displayer);
+
+    // example 3
+
+    session
+    .query_unpaged(
+        "CREATE TABLE IF NOT EXISTS examples_ks.basic6 (a int, timud timeuuid, date1 date, ipaddr inet, dur duration, primary key (a))",
+        &[],
+    )
+    .await?;
+
+    session
+    .query_unpaged(
+        "INSERT INTO examples_ks.basic6 
+        (a, timud, date1, ipaddr, dur) 
+        VALUES 
+        (1, now(), '2021-01-01', '3.14.15.9', 1h);",
+        &[],
+    )
+    .await?;
+
+    session
+    .query_unpaged(
+        "INSERT INTO examples_ks.basic6 
+        (a, timud, date1, ipaddr, dur) 
+        VALUES
+        (3, NOW(), '2024-01-15', '128.0.0.1', 89h4m48s137ms);", // cqlsh prints this as 89.08003805555556h4.8022833333333335m48.137s137.0ms
+        &[],
+    )
+    .await?;
+
+    session
+    .query_unpaged(
+        "INSERT INTO examples_ks.basic6 
+        (a, timud, date1, ipaddr, dur) 
+        VALUES
+        (4, NOW(), '2024-01-15', '192.168.0.14', 13y2w89h4m48s137ms);", // cqlsh prints this as 89.08003805555556h4.8022833333333335m48.137s137.0ms
+        &[],
+    )
+    .await?;
+
+    session
+    .query_unpaged(
+        "INSERT INTO examples_ks.basic6 (a, timud, date1, ipaddr, dur) 
+        VALUES (2, NOW(), '2024-02-20', '2001:0db8:0:0::1428:57ab', 5d2h);",
+        &[],
+    )
+    .await?;
+
+    session
+    .query_unpaged(
+        "INSERT INTO examples_ks.basic6 (a, timud, date1, ipaddr, dur) 
+        VALUES (5, NOW(), '-250000-02-20', '2001:db8::1428:57ab', 1y1mo1w1d1h1m1s700ms);",
+        &[],
+    )
+    .await?;
+
+    let result2 : QueryRowsResult= session
+        .query_unpaged("SELECT * FROM examples_ks.basic6", &[])
+        .await?
+        .into_rows_result()?;
+   
+    let displayer = result2.rows_displayer();
+    println!("DISPLAYER:");
+    println!("{}", displayer);
     Ok(())
 }
