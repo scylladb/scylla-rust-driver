3d711cfa53edf2bf787fa47de027583e6710c70e
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-01-01 19:25:00 +0100

    execution: add module docstring

--- scylla/src/session/mod.rs
+++ scylla/src/session/mod.rs
@@ -1,3 +1,14 @@
+//! This module holds entities that represent the whole configurable
+//! driver session with the cluster.
+//! The following abstractions are involved:
+//! - Session - the main entity of the driver. It:
+//!     - contains and manages all driver configuration,
+//!     - launches and communicates with ClusterWorker (see cluster module for more info),
+//!     - enables executing CQL requests, taking all configuration into consideration.
+//! - SessionBuilder - just a convenient builder for a Session.
+//! - CachingSession - a wrapper over a Session that keeps and manages a cache
+//!   of prepared statements, so that a user can be free of such considerations.
+
 mod caching_session;
 #[allow(clippy::module_inception)]
 mod session;
