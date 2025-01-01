14a6e83f261989585e19072f93559dabe89ab703
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-01-01 19:05:28 +0100

    cluster: add module docstring

--- scylla/src/cluster/mod.rs
+++ scylla/src/cluster/mod.rs
@@ -1,3 +1,20 @@
+//! This module holds entities that represent the cluster as a whole,
+//! nodes in the cluster (together with a pool of connections),
+//! the cluster's state, and logic for ruling out specific nodes.
+//!
+//! This includes:
+//! - node's representation (Node),
+//! - metadata representation, fetching and management, including:
+//!     - topology metadata,
+//!     - schema metadata,
+//!     - tablet metadata,
+//! - ClusterData, which is a snapshot of the cluster's state.
+//!   - ClusterData is replaced atomically upon a metadata refresh,
+//!     preventing any issues arising from mutability, including races.
+//! - HostFilter, which is a way to filter out some nodes and thus
+//!   not contact them at all on any condition.
+//!
+
 #[allow(clippy::module_inception)]
 mod cluster;
 pub mod host_filter;
