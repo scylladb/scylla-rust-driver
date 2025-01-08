ea2f5f0d403be454d779c6b1f7455462e9740eea
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-01-08 17:41:16 +0100

    cluster: rename cluster.rs -> worker.rs
    This way we avoid the module (name) inception.

--- scylla/src/cluster/mod.rs
+++ scylla/src/cluster/mod.rs
@@ -1,10 +1,5 @@
-// The purpose of cluster::cluster module is to not have any complex logic in mod.rs.
-// No code external to this module will ever see this awkward path, because the inner
-// cluster module is pub(self), and its items are only accessible through the below
-// re-exports.
-#[allow(clippy::module_inception)]
-mod cluster;
-pub(crate) use cluster::{use_keyspace_result, Cluster, ClusterNeatDebug};
+mod worker;
+pub(crate) use worker::{use_keyspace_result, Cluster, ClusterNeatDebug};
 
 mod state;
 pub use state::ClusterState;
