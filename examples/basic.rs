1dbeaed1dd76859b5a39d7d396e445fefd908403
Dawid Pawlik <501149991dp@gmail.com>
2025-01-12 18:51:44 +0100

    docs: adjust metrics documentation
    Inform that metrics may now only be used under crate feature 'metrics'.
    Mention new metrics in documentation and show an example
    how to collect them.
    Adjust examples to include new metrics.

--- examples/basic.rs
+++ examples/basic.rs
@@ -94,11 +94,32 @@ async fn main() -> Result<()> {
     println!("Iter queries requested: {}", metrics.get_queries_iter_num());
     println!("Errors occurred: {}", metrics.get_errors_num());
     println!("Iter errors occurred: {}", metrics.get_errors_iter_num());
-    println!("Average latency: {}", metrics.get_latency_avg_ms().unwrap());
+    println!("Average latency: {}", metrics.get_latency_avg_ms()?);
     println!(
         "99.9 latency percentile: {}",
-        metrics.get_latency_percentile_ms(99.9).unwrap()
+        metrics.get_latency_percentile_ms(99.9)?
     );
+
+    let snapshot = metrics.get_snapshot()?;
+    println!("Min: {}", snapshot.min);
+    println!("Max: {}", snapshot.max);
+    println!("Mean: {}", snapshot.mean);
+    println!("Standard deviation: {}", snapshot.stddev);
+    println!("Median: {}", snapshot.median);
+    println!("75th percentile: {}", snapshot.percentile_75);
+    println!("95th percentile: {}", snapshot.percentile_95);
+    println!("98th percentile: {}", snapshot.percentile_98);
+    println!("99th percentile: {}", snapshot.percentile_99);
+    println!("99.9th percentile: {}", snapshot.percentile_99_9);
+
+    println!("Mean rate: {}", metrics.get_mean_rate());
+    println!("One minute rate: {}", metrics.get_one_minute_rate());
+    println!("Five minute rate: {}", metrics.get_five_minute_rate());
+    println!("Fifteen minute rate: {}", metrics.get_fifteen_minute_rate());
+
+    println!("Total connections: {}", metrics.get_total_connections());
+    println!("Connection timeouts: {}", metrics.get_connection_timeouts());
+    println!("Requests timeouts: {}", metrics.get_request_timeouts());
 
     println!("Ok.");
 
