222ea9ce3af453a2d68a95ea046b7d74096a902a
Karol Baryła <karol.baryla@scylladb.com>
2025-01-22 14:32:03 +0100

    Deser macros: fully qualified call to "Clone::clone()"
    Macros need to use fully qualified paths in order to maintain hygiene.

--- scylla-macros/src/deserialize/value.rs
+++ scylla-macros/src/deserialize/value.rs
@@ -367,7 +367,7 @@ impl TypeCheckAssumeOrderGenerator<'_> {
                         required_fields: ::std::vec![
                             #(stringify!(#required_fields_names),)*
                         ],
-                        present_fields: ::std::iter::Iterator::collect(::std::iter::Iterator::map(fields.iter(), |(name, _typ)| name.clone().into_owned())),
+                        present_fields: ::std::iter::Iterator::collect(::std::iter::Iterator::map(fields.iter(), |(name, _typ)| ::std::clone::Clone::clone(name).into_owned())),
                     }
                 );
 
