f078156b84fd9b79a8e1e6b4a6cdbbb5615103b8
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-02-01 21:26:26 +0100

    cloud: refactor `set_ssl_config_for_scylla_cloud_host`
    This step improves types safety and prepares for next steps.

--- scylla/src/cloud/mod.rs
+++ scylla/src/cloud/mod.rs
@@ -8,27 +8,14 @@ use tracing::warn;
 use uuid::Uuid;
 
 use crate::client::session::TlsContext;
-use crate::network::{ConnectionConfig, TlsConfig, TlsError};
+use crate::network::{TlsConfig, TlsError};
 
-pub(crate) fn set_tls_config_for_scylla_cloud_host(
+pub(crate) fn make_tls_config_for_scylla_cloud_host(
     host_id: Option<Uuid>,
     dc: Option<&str>,
     proxy_address: SocketAddr,
-    connection_config: &mut ConnectionConfig,
-) -> Result<(), TlsError> {
-    if connection_config.tls_config.is_some() {
-        // This can only happen if the user builds SessionConfig by hand, as SessionBuilder in cloud mode prevents setting custom TlsContext.
-        warn!(
-            "Overriding user-provided TlsContext with Scylla Cloud TlsContext due \
-                to CloudConfig being provided. This is certainly an API misuse - Cloud \
-                may not be combined with user's own TLS config."
-        )
-    }
-
-    let cloud_config = connection_config
-        .cloud_config
-        .as_deref()
-        .expect("BUG: CloudConfig presence in ConnectionConfig should have been checked before calling this function");
+    cloud_config: &CloudConfig,
+) -> Result<Option<TlsConfig>, TlsError> {
     let datacenter = dc.and_then(|dc| cloud_config.get_datacenters().get(dc));
     if let Some(datacenter) = datacenter {
         let domain_name = datacenter.get_node_domain();
@@ -53,11 +40,15 @@ pub(crate) fn set_tls_config_for_scylla_cloud_host(
             }
         };
 
-        let tls_config = TlsConfig::new_for_sni(tls_context, domain_name, host_id);
-        connection_config.tls_config = Some(tls_config);
+        Ok(Some(TlsConfig::new_for_sni(
+            tls_context,
+            domain_name,
+            host_id,
+        )))
     } else {
         warn!("Datacenter {:?} of node {:?} with addr {} not described in cloud config. Proceeding without setting SNI for the node, which will most probably result in nonworking connections,.",
                dc, host_id, proxy_address);
+        // FIXME: Consider returning error here.
+        Ok(None)
     }
-    Ok(())
 }
