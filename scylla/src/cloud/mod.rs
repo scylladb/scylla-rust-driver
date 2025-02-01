7574af6c0fac57df77edc3728717b72764a8639e
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-02-01 21:53:52 +0100

    cloud: refactor `make_tls_config_for_scylla_cloud_host`
    This just changes the style from `if let` to `let else` for better
    readability.
    
    This commit should be viewed without whitespace difference.

--- scylla/src/cloud/mod.rs
+++ scylla/src/cloud/mod.rs
@@ -16,39 +16,38 @@ pub(crate) fn make_tls_config_for_scylla_cloud_host(
     proxy_address: SocketAddr,
     cloud_config: &CloudConfig,
 ) -> Result<Option<TlsConfig>, TlsError> {
-    let datacenter = dc.and_then(|dc| cloud_config.get_datacenters().get(dc));
-    if let Some(datacenter) = datacenter {
-        let domain_name = datacenter.get_node_domain();
-        let auth_info = cloud_config.get_current_auth_info();
-
-        let tls_context = match auth_info.get_tls() {
-            #[cfg(feature = "openssl-010")]
-            config::TlsInfo::OpenSsl010 { key, cert } => {
-                use openssl::ssl::{SslContext, SslMethod, SslVerifyMode};
-                let mut builder = SslContext::builder(SslMethod::tls())?;
-                builder.set_verify(if datacenter.get_insecure_skip_tls_verify() {
-                    SslVerifyMode::NONE
-                } else {
-                    SslVerifyMode::PEER
-                });
-                let ca = datacenter.openssl_ca();
-                builder.cert_store_mut().add_cert(ca.clone())?;
-                builder.set_certificate(cert)?;
-                builder.set_private_key(key)?;
-                let context = builder.build();
-                TlsContext::OpenSsl010(context)
-            }
-        };
-
-        Ok(Some(TlsConfig::new_for_sni(
-            tls_context,
-            domain_name,
-            host_id,
-        )))
-    } else {
+    let Some(datacenter) = dc.and_then(|dc| cloud_config.get_datacenters().get(dc)) else {
         warn!("Datacenter {:?} of node {:?} with addr {} not described in cloud config. Proceeding without setting SNI for the node, which will most probably result in nonworking connections,.",
                dc, host_id, proxy_address);
         // FIXME: Consider returning error here.
-        Ok(None)
-    }
+        return Ok(None);
+    };
+
+    let domain_name = datacenter.get_node_domain();
+    let auth_info = cloud_config.get_current_auth_info();
+
+    let tls_context = match auth_info.get_tls() {
+        #[cfg(feature = "openssl-010")]
+        config::TlsInfo::OpenSsl010 { key, cert } => {
+            use openssl::ssl::{SslContext, SslMethod, SslVerifyMode};
+            let mut builder = SslContext::builder(SslMethod::tls())?;
+            builder.set_verify(if datacenter.get_insecure_skip_tls_verify() {
+                SslVerifyMode::NONE
+            } else {
+                SslVerifyMode::PEER
+            });
+            let ca = datacenter.openssl_ca();
+            builder.cert_store_mut().add_cert(ca.clone())?;
+            builder.set_certificate(cert)?;
+            builder.set_private_key(key)?;
+            let context = builder.build();
+            TlsContext::OpenSsl010(context)
+        }
+    };
+
+    Ok(Some(TlsConfig::new_for_sni(
+        tls_context,
+        domain_name,
+        host_id,
+    )))
 }
