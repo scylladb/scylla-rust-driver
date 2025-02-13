126cb66ea0772e4ce111fce1ebf235ede6efbcfc
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-02-12 23:38:32 -0400

    add custom condition

--- scylla-proxy/src/lib.rs
+++ scylla-proxy/src/lib.rs
@@ -6,11 +6,11 @@ mod proxy;
 pub type TargetShard = u16;
 
 pub use actions::{
-    example_db_errors, Action, Condition, Reaction, RequestReaction, RequestRule, ResponseReaction,
-    ResponseRule,
+    example_db_errors, Action, Condition, ConditionHandler, Reaction, RequestReaction, RequestRule,
+    ResponseReaction, ResponseRule,
 };
 pub use errors::{DoorkeeperError, ProxyError, WorkerError};
-pub use frame::{RequestFrame, RequestOpcode, ResponseFrame, ResponseOpcode};
+pub use frame::{FrameOpcode, RequestFrame, RequestOpcode, ResponseFrame, ResponseOpcode};
 pub use proxy::{Node, Proxy, RunningProxy, ShardAwareness};
 
 pub use proxy::get_exclusive_local_address;
