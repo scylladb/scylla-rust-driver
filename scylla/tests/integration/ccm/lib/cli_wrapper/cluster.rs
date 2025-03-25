3a83653e6c439df2202dd753effabb7c9b9d7fa1
Karol Baryła <karol.baryla@scylladb.com>
2025-03-25 14:10:50 +0100

    Add CCM integration and example test
    Co-authored-by: Mikołaj Uzarski <mikolaj.uzarski@scylladb.com>
    Co-authored-by: Dmitry Kropachev <dmitry.kropachev@gmail.com>

---
+++ scylla/tests/integration/ccm/lib/cli_wrapper/cluster.rs
@@ -0,0 +1,330 @@
+use std::collections::HashMap;
+use std::fmt::Write;
+use std::path::Path;
+use std::process::ExitStatus;
+use std::sync::Arc;
+
+use itertools::Itertools;
+
+use crate::ccm::lib::ip_allocator::NetPrefix;
+use crate::ccm::lib::logged_cmd::{LoggedCmd, RunOptions};
+
+use super::node::NodeCcm;
+use super::{DBType, NodeStartOptions};
+
+pub(crate) struct Ccm {
+    config_dir: String,
+    cmd: Arc<LoggedCmd>,
+}
+
+impl Ccm {
+    pub(crate) fn new(config_dir: impl AsRef<Path>, cmd: Arc<LoggedCmd>) -> Self {
+        Self {
+            config_dir: config_dir
+                .as_ref()
+                .to_str()
+                .expect("Config dir not representable as str")
+                .to_string(),
+            cmd,
+        }
+    }
+
+    pub(crate) fn for_node(&self, node_name: String) -> NodeCcm {
+        NodeCcm::new(self.config_dir.clone(), Arc::clone(&self.cmd), node_name)
+    }
+}
+
+///
+/// ccm create
+///
+pub(crate) struct ClusterCreate<'ccm> {
+    ccm: &'ccm mut Ccm,
+    name: String,
+    version: String,
+    db_type: DBType,
+    ip_prefix: Option<String>,
+    nodes: Option<String>,
+}
+
+impl Ccm {
+    pub(crate) fn cluster_create(
+        &mut self,
+        name: String,
+        version: String,
+        db_type: DBType,
+    ) -> ClusterCreate<'_> {
+        ClusterCreate {
+            ccm: self,
+            name,
+            version,
+            db_type,
+            ip_prefix: None,
+            nodes: None,
+        }
+    }
+}
+
+impl ClusterCreate<'_> {
+    pub(crate) fn populate(mut self, nodes: &[u8], ipprefix: Option<NetPrefix>) -> Self {
+        let nodes_string = nodes.iter().map(|node| node.to_string()).join(":");
+        self.nodes = Some(nodes_string);
+        self.ip_prefix = ipprefix.map(|p| p.to_string());
+        self
+    }
+
+    pub(crate) async fn run(self) -> Result<ExitStatus, anyhow::Error> {
+        let mut args: Vec<&str> = vec![
+            "create",
+            self.name.as_str(),
+            "--version",
+            self.version.as_str(),
+            "--config-dir",
+            self.ccm.config_dir.as_str(),
+        ];
+        if let Some(nodes) = self.nodes.as_ref() {
+            args.push("--nodes");
+            args.push(nodes.as_str())
+        }
+        if let Some(ipprefix) = self.ip_prefix.as_ref() {
+            args.push("--ipprefix");
+            args.push(ipprefix.as_str());
+        }
+        if self.db_type == DBType::Scylla {
+            args.push("--scylla");
+        }
+        self.ccm
+            .cmd
+            .run_command("ccm", &args, RunOptions::new())
+            .await
+    }
+}
+
+///
+/// ccm updateconf
+///
+pub(crate) struct ClusterUpdateconf<'ccm> {
+    ccm: &'ccm mut Ccm,
+    args: Vec<String>,
+}
+
+impl Ccm {
+    pub(crate) fn cluster_updateconf(&mut self) -> ClusterUpdateconf<'_> {
+        let cfg_dir = self.config_dir.as_str().to_string();
+        ClusterUpdateconf {
+            ccm: self,
+            args: vec![
+                "updateconf".to_string(),
+                "--config-dir".to_string(),
+                cfg_dir,
+            ],
+        }
+    }
+}
+
+impl ClusterUpdateconf<'_> {
+    pub(crate) fn config<K, V>(mut self, key_values: impl IntoIterator<Item = (K, V)>) -> Self
+    where
+        K: AsRef<str>,
+        V: AsRef<str>,
+    {
+        for (k, v) in key_values.into_iter() {
+            self.args.push(format!("{}:{}", k.as_ref(), v.as_ref()));
+        }
+        self
+    }
+    pub(crate) async fn run(self) -> Result<ExitStatus, anyhow::Error> {
+        self.ccm
+            .cmd
+            .run_command("ccm", &self.args, RunOptions::new())
+            .await
+    }
+}
+
+///
+/// ccm start
+///
+pub(crate) struct ClusterStart<'ccm> {
+    ccm: &'ccm mut Ccm,
+    wait_opts: Option<NodeStartOptions>,
+    scylla_smp: Option<u16>,
+    scylla_mem_megabytes: Option<u32>,
+}
+
+impl Ccm {
+    pub(crate) fn cluster_start(&mut self) -> ClusterStart<'_> {
+        ClusterStart {
+            ccm: self,
+            wait_opts: None,
+            scylla_smp: None,
+            scylla_mem_megabytes: None,
+        }
+    }
+}
+
+impl ClusterStart<'_> {
+    pub(crate) fn wait_options(mut self, options: Option<NodeStartOptions>) -> Self {
+        self.wait_opts = options;
+        self
+    }
+    pub(crate) fn scylla_smp(mut self, smp: u16) -> Self {
+        self.scylla_smp = Some(smp);
+        self
+    }
+
+    pub(crate) fn scylla_mem_megabytes(mut self, mem_megabytes: u32) -> Self {
+        self.scylla_mem_megabytes = Some(mem_megabytes);
+        self
+    }
+
+    fn get_ccm_env(&self) -> HashMap<String, String> {
+        let mut value = String::new();
+        if let Some(smp) = self.scylla_smp {
+            write!(value, "--smp={} ", smp).unwrap();
+        }
+        if let Some(mem_megabytes) = self.scylla_mem_megabytes {
+            write!(value, "--memory={}M ", mem_megabytes).unwrap();
+        }
+
+        let mut env = HashMap::new();
+        if !value.is_empty() {
+            env.insert("SCYLLA_EXT_OPTS".to_string(), value);
+        }
+
+        env
+    }
+
+    pub(crate) async fn run(self) -> Result<ExitStatus, anyhow::Error> {
+        let env = self.get_ccm_env();
+        let mut args = vec!["start", "--config-dir", self.ccm.config_dir.as_str()];
+
+        let NodeStartOptions {
+            no_wait,
+            wait_other_notice,
+            wait_for_binary_proto,
+        } = self.wait_opts.unwrap_or_default();
+        if no_wait {
+            args.push(NodeStartOptions::NO_WAIT);
+        }
+        if wait_other_notice {
+            args.push(NodeStartOptions::WAIT_OTHER_NOTICE);
+        }
+        if wait_for_binary_proto {
+            args.push(NodeStartOptions::WAIT_FOR_BINARY_PROTO);
+        }
+
+        self.ccm
+            .cmd
+            .run_command("ccm", &args, RunOptions::new().with_env(env))
+            .await
+    }
+}
+
+///
+/// ccm remove
+///
+pub(crate) struct ClusterRemove<'ccm> {
+    ccm: &'ccm mut Ccm,
+}
+
+impl Ccm {
+    pub(crate) fn cluster_remove(&mut self) -> ClusterRemove<'_> {
+        ClusterRemove { ccm: self }
+    }
+}
+
+impl ClusterRemove<'_> {
+    pub(crate) async fn run(self) -> Result<ExitStatus, anyhow::Error> {
+        let args = &["remove", "--config-dir", self.ccm.config_dir.as_str()];
+
+        self.ccm
+            .cmd
+            .run_command("ccm", args, RunOptions::new())
+            .await
+    }
+
+    pub(crate) fn run_sync(self) -> Result<ExitStatus, anyhow::Error> {
+        let args = &["remove", "--config-dir", self.ccm.config_dir.as_str()];
+
+        self.ccm
+            .cmd
+            .run_command_sync("ccm", args, RunOptions::new())
+    }
+}
+
+///
+/// ccm stop
+///
+pub(crate) struct ClusterStop<'ccm> {
+    ccm: &'ccm mut Ccm,
+}
+
+impl Ccm {
+    pub(crate) fn cluster_stop(&mut self) -> ClusterStop<'_> {
+        ClusterStop { ccm: self }
+    }
+}
+
+impl ClusterStop<'_> {
+    pub(crate) async fn run(self) -> Result<ExitStatus, anyhow::Error> {
+        let args = &["stop", "--config-dir", self.ccm.config_dir.as_str()];
+
+        self.ccm
+            .cmd
+            .run_command("ccm", args, RunOptions::new())
+            .await
+    }
+}
+
+///
+/// ccm add
+///
+pub(crate) struct ClusterAddNode<'ccm> {
+    ccm: &'ccm mut Ccm,
+    name: String,
+    db_type: DBType,
+    dc: Option<String>,
+}
+
+impl Ccm {
+    pub(crate) fn cluster_add_node(&mut self, db_type: DBType, name: String) -> ClusterAddNode<'_> {
+        ClusterAddNode {
+            ccm: self,
+            db_type,
+            name,
+            dc: None,
+        }
+    }
+}
+
+impl ClusterAddNode<'_> {
+    pub(crate) fn dc(mut self, dc: String) -> Self {
+        self.dc = Some(dc);
+        self
+    }
+
+    pub(crate) async fn run(self) -> Result<ExitStatus, anyhow::Error> {
+        let mut args = vec![
+            "add",
+            "--config-dir",
+            self.ccm.config_dir.as_str(),
+            self.name.as_str(),
+        ];
+
+        if let Some(dc) = self.dc.as_ref() {
+            args.push("--data-center");
+            args.push(dc.as_str())
+        }
+
+        match self.db_type {
+            DBType::Scylla => {
+                args.push("--scylla");
+            }
+            DBType::Cassandra => {}
+        }
+
+        self.ccm
+            .cmd
+            .run_command("ccm", args, RunOptions::new())
+            .await
+    }
+}
---
+++ scylla/tests/integration/ccm/lib/node.rs
@@ -0,0 +1,182 @@
+use std::net::IpAddr;
+
+use anyhow::Error;
+
+use super::cli_wrapper::node::NodeCcm;
+use super::cli_wrapper::{DBType, NodeStartOptions, NodeStopOptions};
+use super::cluster::ClusterOptions;
+use super::ip_allocator::NetPrefix;
+
+pub(crate) type NodeId = u16;
+
+#[derive(Debug, Clone)]
+pub(crate) struct NodeOptions {
+    /// Node ID, needed to compose ccm commands properly
+    pub(crate) id: NodeId,
+    /// Database Type: Cassandra, Scylla or Datastax
+    #[expect(dead_code)]
+    pub(crate) db_type: DBType,
+    /// Scylla or Cassandra version string that goes to CCM.
+    /// Examples: `release:6.2.2`, `unstable:master/2021-05-24T17:16:53Z`
+    #[expect(dead_code)]
+    pub(crate) version: String,
+    /// Datacenter ID
+    #[expect(dead_code)]
+    pub(crate) datacenter_id: u16,
+    /// CCM allocates node ip addresses based on this prefix:
+    /// if ip_prefix = `127.0.1.`, then `node1` address is `127.0.1.1`, `node2` address is `127.0.1.2`
+    pub(crate) ip_prefix: NetPrefix,
+    /// Number of vCPU for Scylla to occupy
+    pub(crate) smp: u16,
+    /// Amount of MB for Scylla to occupy. Has to be bigger than `smp`*512.
+    pub(crate) memory: u32,
+}
+
+impl NodeOptions {
+    pub(crate) fn name(&self) -> String {
+        format!("node{}", self.id)
+    }
+
+    pub(super) fn from_cluster_opts(value: &ClusterOptions) -> Self {
+        NodeOptions {
+            id: 0,
+            datacenter_id: 1,
+            db_type: value.db_type,
+            version: value.version.clone(),
+            ip_prefix: value.ip_prefix,
+            smp: value.smp,
+            memory: value.memory,
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub(crate) enum NodeStatus {
+    Stopped,
+    Started,
+    Deleted,
+}
+
+pub(crate) struct Node {
+    status: NodeStatus,
+    opts: NodeOptions,
+    ccm_cmd: NodeCcm,
+}
+
+impl Node {
+    pub(super) fn new(opts: NodeOptions, ccm_cmd: NodeCcm) -> Self {
+        Node {
+            opts,
+            status: NodeStatus::Stopped,
+            ccm_cmd,
+        }
+    }
+
+    #[expect(dead_code)]
+    pub(crate) fn name(&self) -> String {
+        self.opts.name()
+    }
+
+    pub(crate) fn id(&self) -> NodeId {
+        self.opts.id
+    }
+
+    pub(crate) fn contact_endpoint(&self) -> String {
+        format!(
+            "{}:{}",
+            self.broadcast_rpc_address(),
+            self.native_transport_port()
+        )
+    }
+
+    pub(crate) fn broadcast_rpc_address(&self) -> IpAddr {
+        self.opts.ip_prefix.to_ipaddress(self.opts.id)
+    }
+
+    pub(crate) fn native_transport_port(&self) -> u16 {
+        9042
+    }
+
+    /// Executes `ccm updateconf` and applies it for this node.
+    /// It accepts the key-value pairs to update the configuration.
+    ///
+    /// ### Example
+    /// ```
+    /// # use crate::ccm::cluster::Node;
+    /// # async fn check_only_compiles(node: &Node) -> Result<(), Box<dyn Error>> {
+    /// let args = [
+    ///     ("client_encryption_options.enabled", "true"),
+    ///     ("client_encryption_options.certificate", "db.cert"),
+    ///     ("client_encryption_options.keyfile", "db.key"),
+    /// ];
+    ///
+    /// node.updateconf(args).await?
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// The code above is equivalent to the following scylla.yaml:
+    /// ```yaml
+    /// client_encryption_options:
+    ///   enabled: true
+    ///   certificate: db.cert
+    ///   keyfile: db.key
+    /// ```
+    #[expect(dead_code)]
+    pub(crate) async fn updateconf<K, V>(
+        &mut self,
+        key_values: impl IntoIterator<Item = (K, V)>,
+    ) -> Result<(), Error>
+    where
+        K: AsRef<str>,
+        V: AsRef<str>,
+    {
+        self.ccm_cmd
+            .node_updateconf()
+            .config(key_values)
+            .run()
+            .await
+            .map(|_| ())
+    }
+
+    /// This method starts the node. User can provide optional [`NodeStartOptions`] to control the behavior of the node start.
+    /// If `None` is provided, the default options are used (see the implementation of Default for [`NodeStartOptions`]).
+    #[expect(dead_code)]
+    pub(crate) async fn start(&mut self, opts: Option<NodeStartOptions>) -> Result<(), Error> {
+        self.ccm_cmd
+            .node_start()
+            .wait_options(opts)
+            .scylla_smp(self.opts.smp)
+            .scylla_mem_megabytes(self.opts.memory)
+            .run()
+            .await?;
+        self.set_status(NodeStatus::Started);
+        Ok(())
+    }
+
+    #[expect(dead_code)]
+    pub(crate) async fn stop(&mut self, opts: Option<NodeStopOptions>) -> Result<(), Error> {
+        self.ccm_cmd.node_stop().wait_options(opts).run().await?;
+        self.set_status(NodeStatus::Stopped);
+        Ok(())
+    }
+
+    #[expect(dead_code)]
+    pub(crate) async fn delete(&mut self) -> Result<(), Error> {
+        if self.status == NodeStatus::Deleted {
+            return Ok(());
+        }
+        self.ccm_cmd.node_remove().run().await?;
+        self.set_status(NodeStatus::Deleted);
+        Ok(())
+    }
+
+    pub(super) fn set_status(&mut self, status: NodeStatus) {
+        self.status = status;
+    }
+
+    #[expect(dead_code)]
+    pub(crate) fn status(self) -> NodeStatus {
+        self.status
+    }
+}
