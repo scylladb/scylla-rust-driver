86d8fe0fb169b471889c68fcfaaf5e323c212dd9
Andres Medina <andresnrx@gmail.com>
2025-04-11 11:02:24 -0700

    Allow adding statements to a boundbatch and executing it

--- scylla/src/network/connection.rs
+++ scylla/src/network/connection.rs
@@ -1114,7 +1114,7 @@ impl Connection {
             consistency,
             serial_consistency,
             timestamp,
-            statements_len: batch.statements_len,
+            statements_len: batch.statements_len(),
         };
 
         loop {
