999d74eb75f740ae6581a00dc7c71b2497d86b00
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-05-08 14:34:27 +0200

    connection_pool: include `endpoint` field in Debug impl
    When two years ago I added the `endpoint` field to `NodeConnectionPool`,
    I forgot to include it in its custom `Debug` impl. Now it's fixed.

--- scylla/src/network/connection_pool.rs
+++ scylla/src/network/connection_pool.rs
@@ -186,6 +186,7 @@ impl std::fmt::Debug for NodeConnectionPool {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("NodeConnectionPool")
             .field("conns", &self.conns)
+            .field("endpoint", &self.endpoint)
             .finish_non_exhaustive()
     }
 }
