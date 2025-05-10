2447de1a9aaec139b7e9efb02fb78652743e733e
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-05-10 09:24:08 +0200

    integration: extract keyspace_filtering.rs
    Test `test_keyspaces_to_fetch()` is extracted from `session/session.rs`
    and now comprises `metadata/keyspace_filtering.rs`.

--- scylla/tests/integration/metadata/mod.rs
+++ scylla/tests/integration/metadata/mod.rs
@@ -1,4 +1,5 @@
 mod contents;
+mod keyspace_filtering;
 mod metadata_custom_timeouts;
 mod refresh_upon_schema_agreement;
 mod turn_off_schema_fetching;
--- scylla/tests/integration/session/session.rs
+++ scylla/tests/integration/session/session.rs
@@ -1421,45 +1421,6 @@ async fn test_rate_limit_exceeded_exception() {
     }
 }
 
-#[tokio::test]
-async fn test_keyspaces_to_fetch() {
-    let ks1 = unique_keyspace_name();
-    let ks2 = unique_keyspace_name();
-
-    let session_default = create_new_session_builder().build().await.unwrap();
-    for ks in [&ks1, &ks2] {
-        session_default
-            .ddl(format!("CREATE KEYSPACE {} WITH REPLICATION = {{'class' : 'NetworkTopologyStrategy', 'replication_factor' : 1}}", ks))
-            .await
-            .unwrap();
-    }
-    session_default.await_schema_agreement().await.unwrap();
-    assert!(session_default
-        .get_cluster_state()
-        .get_keyspace(&ks1)
-        .is_some());
-    assert!(session_default
-        .get_cluster_state()
-        .get_keyspace(&ks2)
-        .is_some());
-
-    let session1 = create_new_session_builder()
-        .keyspaces_to_fetch([&ks1])
-        .build()
-        .await
-        .unwrap();
-    assert!(session1.get_cluster_state().get_keyspace(&ks1).is_some());
-    assert!(session1.get_cluster_state().get_keyspace(&ks2).is_none());
-
-    let session_all = create_new_session_builder()
-        .keyspaces_to_fetch([] as [String; 0])
-        .build()
-        .await
-        .unwrap();
-    assert!(session_all.get_cluster_state().get_keyspace(&ks1).is_some());
-    assert!(session_all.get_cluster_state().get_keyspace(&ks2).is_some());
-}
-
 // Reproduces the problem with execute_iter mentioned in #608.
 #[tokio::test]
 async fn test_iter_works_when_retry_policy_returns_ignore_write_error() {
