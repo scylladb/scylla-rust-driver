420033e4a9d9fd25adc6956b605b6e25bc5ca720
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-05-10 09:51:48 +0200

    integration: extract test_batch_to_multiple_tables()
    The test is extracted into `statements/batch/multi_table_batch.rs`.

--- scylla/tests/integration/statements/batch/mod.rs
+++ scylla/tests/integration/statements/batch/mod.rs
@@ -2,6 +2,7 @@ mod basic;
 mod batch_lwts;
 mod counter_batch;
 mod large_batch_statements;
+mod multi_table_batch;
 mod prepare_batch;
 mod silent_prepare_batch;
 mod statements_values_count_mismatch;
