414f7ef8572f3d48a978d47131b5c38a6888c5b2
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-05-10 11:11:20 +0200

    integration: add test case for exposed Coordinator
    The test checks that if a node is enforced as the coordinator
    of a request, the [Coordinator] struct exposed on the request result
    (`QueryResult` and `QueryPager`) contains that `Node`.
    
    Fixes: #1347

--- scylla/tests/integration/statements/coordinator.rs
+++ scylla/tests/integration/statements/coordinator.rs
@@ -8,6 +8,8 @@ use scylla::client::session::Session;
 use scylla::errors::ExecutionError;
 
 use scylla::policies::load_balancing::{NodeIdentifier, SingleTargetLoadBalancingPolicy};
+use scylla::response::Coordinator;
+use scylla::routing::Shard;
 use scylla::statement::Statement;
 use uuid::Uuid;
 
@@ -105,4 +107,75 @@ async fn test_enforce_non_existent_request_coordinator() {
 
     let result = session.query_unpaged(statement, ()).await;
     assert_matches!(result, Err(ExecutionError::EmptyPlan))
+}
+
+/// Checks that if a node is enforced as the coordinator of a request, the [Coordinator] struct
+/// exposed on the request result (`QueryResult` and `QueryPager`) contains that `Node`.
+#[tokio::test]
+async fn test_exposed_request_coordinator() {
+    setup_tracing();
+    let session = create_new_session_builder().build().await.unwrap();
+
+    let cluster_state = session.get_cluster_state();
+    for node in cluster_state.get_nodes_info() {
+        let num_shards = node
+            .sharder()
+            .map_or(0, |sharder| sharder.nr_shards.get() as Shard);
+        for requested_shard in std::iter::once(None).chain((0..num_shards).map(Some)) {
+            let mut statement =
+                Statement::new("SELECT host_id, rpc_address FROM system.local WHERE key='local'");
+            statement.set_load_balancing_policy(Some(SingleTargetLoadBalancingPolicy::new(
+                NodeIdentifier::Node(Arc::clone(node)),
+                requested_shard,
+            )));
+
+            let expected_node_id = node.host_id;
+            let expected_node_ip = node.address.ip();
+
+            let check_coordinator = |coordinator: &Coordinator| {
+                assert_eq!(coordinator.node(), node);
+
+                // If we requested no shard, in ScyllaDB we still target some (unspecified) shard, not None.
+                if let Some(shard) = requested_shard {
+                    assert_eq!(coordinator.shard(), Some(shard));
+                }
+            };
+
+            // Check query_unpaged
+            {
+                let query_rows_result = session
+                    .query_unpaged(statement.clone(), ())
+                    .await
+                    .unwrap()
+                    .into_rows_result()
+                    .unwrap();
+
+                let (actual_node_id, actual_node_ip) = query_rows_result
+                    .single_row::<(uuid::Uuid, IpAddr)>()
+                    .unwrap();
+                assert_eq!(expected_node_id, actual_node_id);
+                assert_eq!(expected_node_ip, actual_node_ip);
+
+                let coordinator = query_rows_result.request_coordinator();
+                check_coordinator(coordinator);
+            }
+
+            // Check query_iter
+            {
+                let mut rows_stream = session
+                    .query_iter(statement, ())
+                    .await
+                    .unwrap()
+                    .rows_stream::<(uuid::Uuid, IpAddr)>()
+                    .unwrap();
+
+                let (actual_node_id, actual_node_ip) = rows_stream.next().await.unwrap().unwrap();
+                assert_eq!(expected_node_id, actual_node_id);
+                assert_eq!(expected_node_ip, actual_node_ip);
+
+                let coordinator = rows_stream.request_coordinators().next().unwrap();
+                check_coordinator(coordinator);
+            }
+        }
+    }
 }
