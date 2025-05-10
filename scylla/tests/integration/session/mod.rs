f191efd6a2d793613c7bd4feca8b12e9efeb65d4
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-05-10 10:35:07 +0200

    integration: rename session.rs -> db_errors.rs
    The remaining two tests in session.rs:
    - test_db_errors(),
    - test_rate_limit_exceeded_exception(),
    regard DB errors, so the file is renamed accordingly.
    
    This finishes dissolution of the old overloaded `session_test.rs` module.
    Hooray!

--- scylla/tests/integration/session/mod.rs
+++ scylla/tests/integration/session/mod.rs
@@ -1,11 +1,10 @@
 mod authenticate;
 mod caching_session;
+mod db_errors;
 mod history;
 mod new_session;
 mod pager;
 mod retries;
 mod self_identity;
-#[expect(clippy::module_inception)]
-mod session;
 mod tracing;
 mod use_keyspace;
