10b490d4bbfeec88c8ace5bbd2e20290de0cf6a6
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2025-05-10 09:58:15 +0200

    integration: extract test_prepared_config()
    The test is extracted into `statements/prepared.rs`.

--- scylla/tests/integration/statements/prepared.rs
+++ scylla/tests/integration/statements/prepared.rs
@@ -170,3 +170,28 @@ async fn test_prepared_statement() {
         );
     }
 }
+
+/// Tests that PreparedStatement inherits the StatementConfig from Statement
+/// that it originates from.
+// Commit message:
+//
+// statement: preserve query configuration during prepare()
+// When preparing a statement based on a Query instance, we should preserve the whole
+// configuration. It’s especially important for CachingSession users, because prepare()
+// happens implicitly in this case, so there’s no other way to customize the configuration
+// for a particular query.
+// Fixes #340
+#[tokio::test]
+async fn test_prepared_config() {
+    setup_tracing();
+    let session = create_new_session_builder().build().await.unwrap();
+
+    let mut query = Statement::new("SELECT * FROM system_schema.tables");
+    query.set_is_idempotent(true);
+    query.set_page_size(42);
+
+    let prepared_statement = session.prepare(query).await.unwrap();
+
+    assert!(prepared_statement.get_is_idempotent());
+    assert_eq!(prepared_statement.get_page_size(), 42);
+}
