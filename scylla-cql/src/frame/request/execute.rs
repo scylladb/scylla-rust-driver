2701b8514aec2d775ae4d4ff89ae793cfa481f86
Karol Baryła <karol.baryla@scylladb.com>
2025-10-28 12:51:22 +0100

    scylla-cql: Add ExecuteV2
    We can't add a new field to Execute, so we need to create a new struct.

--- scylla-cql/src/frame/request/execute.rs
+++ scylla-cql/src/frame/request/execute.rs
@@ -3,6 +3,8 @@
 use std::num::TryFromIntError;
 
 use crate::frame::frame_errors::CqlRequestSerializationError;
+use crate::frame::protocol_features::ProtocolFeatures;
+use crate::frame::response::result::cow_bytes::CowBytes;
 use bytes::Bytes;
 use thiserror::Error;
 
@@ -52,7 +54,78 @@ impl DeserializableRequest for Execute<'_> {
     }
 }
 
+/// CQL protocol-level representation of an `EXECUTE` request,
+/// used to execute a single prepared statement.
+#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
+pub struct ExecuteV2<'a> {
+    /// ID of the prepared statement to execute.
+    pub id: CowBytes<'a>,
+
+    /// ID of the result metadata stored locally.
+    pub result_metadata_id: Option<CowBytes<'a>>,
+
+    /// Various parameters controlling the execution of the statement.
+    pub parameters: query::QueryParameters<'a>,
+}
+
+impl SerializableRequest for ExecuteV2<'_> {
+    const OPCODE: RequestOpcode = RequestOpcode::Execute;
+
+    fn serialize(&self, buf: &mut Vec<u8>) -> Result<(), CqlRequestSerializationError> {
+        // Serializing statement id
+        types::write_short_bytes(self.id.as_ref(), buf)
+            .map_err(ExecuteSerializationError::StatementIdSerialization)?;
+
+        // Serializing result metadata id
+        if let Some(id) = self.result_metadata_id.as_ref() {
+            types::write_short_bytes(id.as_ref(), buf)
+                .map_err(ExecuteSerializationError::ResultMetadataIdSerialization)?;
+        }
+
+        // Serializing params
+        self.parameters
+            .serialize(buf)
+            .map_err(ExecuteSerializationError::QueryParametersSerialization)?;
+        Ok(())
+    }
+}
+
+impl DeserializableRequest for ExecuteV2<'static> {
+    fn deserialize(buf: &mut &[u8]) -> Result<Self, RequestDeserializationError> {
+        let id = CowBytes::from(types::read_short_bytes(buf)?).into_owned();
+        let result_metadata_id = CowBytes::from(types::read_short_bytes(buf)?).into_owned();
+        let parameters = QueryParameters::deserialize(buf)?;
+
+        Ok(Self {
+            id,
+            result_metadata_id: Some(result_metadata_id),
+            parameters,
+        })
+    }
+
+    fn deserialize_with_features(
+        buf: &mut &[u8],
+        features: &ProtocolFeatures,
+    ) -> Result<Self, RequestDeserializationError> {
+        let id = CowBytes::from(types::read_short_bytes(buf)?).into_owned();
+        let result_metadata_id = if features.scylla_metadata_id_supported {
+            Some(CowBytes::from(types::read_short_bytes(buf)?).into_owned())
+        } else {
+            None
+        };
+        let parameters = QueryParameters::deserialize(buf)?;
+
+        Ok(Self {
+            id,
+            result_metadata_id,
+            parameters,
+        })
+    }
+}
+
 /// An error type returned when serialization of EXECUTE request fails.
+// TODO(2.0): Remove "Serialization" suffix from error names.
+#[expect(clippy::enum_variant_names)]
 #[non_exhaustive]
 #[derive(Error, Debug, Clone)]
 pub enum ExecuteSerializationError {
@@ -63,4 +136,7 @@ pub enum ExecuteSerializationError {
     /// Failed to serialize prepared statement id.
     #[error("Malformed statement id: {0}")]
     StatementIdSerialization(TryFromIntError),
+
+    #[error("Malformed result metadata id: {0}")]
+    ResultMetadataIdSerialization(TryFromIntError),
 }
