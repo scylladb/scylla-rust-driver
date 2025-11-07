00622d14bef45ecbfd860126b7d8308e401210ab
Karol Baryła <karol.baryla@scylladb.com>
2025-11-07 13:37:30 +0100

    scylla: bump version to 1.4.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Global variables
 
 # Build documentation for the following tags and branches
-TAGS = ['v1.2.0', 'v1.3.0', 'v1.3.1', 'v1.4.0']
+TAGS = ['v1.2.0', 'v1.3.0', 'v1.3.1', 'v1.4.0', 'v1.4.1']
 BRANCHES = ['main']
 # Set the latest version.
-LATEST_VERSION = 'v1.4.0'
+LATEST_VERSION = 'v1.4.1'
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = ['main']
 # Set which versions are deprecated
