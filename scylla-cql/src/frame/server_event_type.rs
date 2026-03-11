5c4df8aab9999e21d6733056302be5c292f680b2
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2026-03-11 07:47:05 +0100

    scylla_cql: introduce EventV2
    This is needed to support CLIENT_ROUTES_CHANGE event, because both Event
    and EventType enums did not have `#[non_exhaustive]` attribute, so
    adding a new variant to it would be a breaking change.
    
    This is required for PrivateLink support.

--- scylla-cql/src/frame/server_event_type.rs
+++ scylla-cql/src/frame/server_event_type.rs
@@ -19,12 +19,42 @@ pub enum EventType {
     SchemaChange,
 }
 
+/// Represents the type of a CQL event.
+// The postfix "Change" is used in all variants just because all currently existing events happen to be
+// about changes in the cluster, so it makes sense to use the same postfix for all of them.
+// If we add a new event type that is not about changes, then clippy will no longer complain.
+#[expect(clippy::enum_variant_names)]
+#[non_exhaustive]
+pub enum EventTypeV2 {
+    /// Represents a change in the cluster topology, such as node addition or removal.
+    TopologyChange,
+    /// Represents a change in the status of a node, such as up or down.
+    StatusChange,
+    /// Represents a change in the schema, such as table creation or modification.
+    SchemaChange,
+    /// Represents a change in the client routes.
+    ClientRoutesChange,
+}
+
 impl fmt::Display for EventType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let s = match &self {
             Self::TopologyChange => "TOPOLOGY_CHANGE",
             Self::StatusChange => "STATUS_CHANGE",
             Self::SchemaChange => "SCHEMA_CHANGE",
+        };
+
+        write!(f, "{s}")
+    }
+}
+
+impl fmt::Display for EventTypeV2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match &self {
+            Self::TopologyChange => "TOPOLOGY_CHANGE",
+            Self::StatusChange => "STATUS_CHANGE",
+            Self::SchemaChange => "SCHEMA_CHANGE",
+            Self::ClientRoutesChange => "CLIENT_ROUTES_CHANGE",
         };
 
         write!(f, "{s}")
@@ -39,6 +69,20 @@ impl FromStr for EventType {
             "TOPOLOGY_CHANGE" => Ok(Self::TopologyChange),
             "STATUS_CHANGE" => Ok(Self::StatusChange),
             "SCHEMA_CHANGE" => Ok(Self::SchemaChange),
+            _ => Err(CqlEventParseError::UnknownEventType(s.to_string())),
+        }
+    }
+}
+
+impl FromStr for EventTypeV2 {
+    type Err = CqlEventParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "TOPOLOGY_CHANGE" => Ok(Self::TopologyChange),
+            "STATUS_CHANGE" => Ok(Self::StatusChange),
+            "SCHEMA_CHANGE" => Ok(Self::SchemaChange),
+            "CLIENT_ROUTES_CHANGE" => Ok(Self::ClientRoutesChange),
             _ => Err(CqlEventParseError::UnknownEventType(s.to_string())),
         }
     }
