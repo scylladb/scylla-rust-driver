8968f391ea27f77e6100abf8319f5b31a80e1d65
Karol Baryła <karol.baryla@scylladb.com>
2026-03-18 15:48:56 +0100

    SerializedValues::write_to_request: Make public
    It is called in some places in scylla-cql. SerializedValues will have to
    be moved to core, so we need to handle it. Publishing the method seems
    easiest, and I don't see any problems with that.

--- scylla-cql/src/serialize/row.rs
+++ scylla-cql/src/serialize/row.rs
@@ -559,7 +559,11 @@ impl SerializedValues {
         self.serialized_values.len()
     }
 
-    pub(crate) fn write_to_request(&self, buf: &mut impl BufMut) {
+    /// Writes the serialized values to the request buffer, including the preceding u16 element count.
+    ///
+    /// It will write a `[short]` N describing the number of elements,
+    /// and then N values, each of which is `[value]` (`[short]` and `[value]` are described by CQL protocol).
+    pub fn write_to_request(&self, buf: &mut impl BufMut) {
         buf.put_u16(self.element_count);
         buf.put(self.serialized_values.as_slice())
     }
