9fb80e64e3b5d271500c2f3b42a492717546bec1
Karol Baryła <karol.baryla@scylladb.com>
2026-03-19 12:32:39 +0100

    scylla-cql/serialization: Re-export whole modules from core

--- scylla-cql/src/serialize/mod.rs
+++ scylla-cql/src/serialize/mod.rs
@@ -2,10 +2,10 @@
 
 //! Types and traits related to serialization of values to the CQL format.
 
-pub mod batch;
+pub use scylla_cql_core::serialize::batch;
 pub mod raw_batch;
-pub mod row;
-pub mod value;
+pub use scylla_cql_core::serialize::row;
+pub use scylla_cql_core::serialize::value;
 pub use scylla_cql_core::serialize::writers;
 
 pub use scylla_cql_core::serialize::SerializationError;
