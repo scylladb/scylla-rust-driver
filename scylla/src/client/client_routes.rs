f163b502643520b211e64dd7915a9151da171a1c
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2026-03-31 12:06:01 +0200

    client: introduce client_routes module
    The module will contain logic that handles connecting to clusters
    via client-routes routing.

---
+++ scylla/src/client/client_routes.rs
@@ -0,0 +1,3 @@
+//! Routing configuration, enabling routing using `system.client_routes`.
+//!
+//! Enables connecting to Scylla Cloud clusters using AWS PrivateLink or GCP Private Service Connect, among others.
