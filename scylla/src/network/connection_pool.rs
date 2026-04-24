dfdc5cdc7ab38ca6618ea73dedeb7dc14d087336
Karol Baryła <karol.baryla@scylladb.com>
2026-04-24 13:45:51 +0200

    test many_connections: Allow subtests
    This commit extracts logic of many_connections tests to a separate
    functions that accepts a connection config. That way it will be easier
    to test various cases with different configs.

--- scylla/src/network/connection_pool.rs
+++ scylla/src/network/connection_pool.rs
@@ -1268,12 +1268,7 @@ mod tests {
     use crate::test_utils::setup_tracing;
     use std::net::{SocketAddr, ToSocketAddrs};
 
-    // Open many connections to a node
-    // Port collision should occur
-    // If they are not handled this test will most likely fail
-    #[tokio::test]
-    async fn many_connections() {
-        setup_tracing();
+    async fn test_many_connections_with_config(connection_config: HostConnectionConfig) {
         let connections_number = 400;
 
         let connect_address: SocketAddr = std::env::var("SCYLLA_URI")
@@ -1282,16 +1277,6 @@ mod tests {
             .unwrap()
             .next()
             .unwrap();
-
-        let connection_config = HostConnectionConfig {
-            compression: None,
-            tcp_socket_options: TcpSocketOptions {
-                nodelay: true,
-                ..Default::default()
-            },
-            tls_config: None,
-            ..Default::default()
-        };
 
         // This does not have to be the real sharder,
         // the test is only about port collisions, not connecting
@@ -1313,5 +1298,24 @@ mod tests {
         for res in joined {
             res.unwrap();
         }
+    }
+
+    // Open many connections to a node
+    // Port collision should occur
+    // If they are not handled this test will most likely fail
+    #[tokio::test]
+    async fn many_connections() {
+        setup_tracing();
+
+        test_many_connections_with_config(HostConnectionConfig {
+            compression: None,
+            tcp_socket_options: TcpSocketOptions {
+                nodelay: true,
+                ..Default::default()
+            },
+            tls_config: None,
+            ..Default::default()
+        })
+        .await;
     }
 }
