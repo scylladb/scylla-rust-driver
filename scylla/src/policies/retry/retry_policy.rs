bacd5f340d8874127c946a4673760dea4ae5ca10
Paulina Czajkowska <paulina.czajkowska04@gmail.com>
2026-05-25 15:29:28 +0200

    feat: add constructor for `RequestInfo`

--- scylla/src/policies/retry/retry_policy.rs
+++ scylla/src/policies/retry/retry_policy.rs
@@ -18,6 +18,22 @@ pub struct RequestInfo<'a> {
     pub consistency: Consistency,
 }
 
+impl<'a> RequestInfo<'a> {
+    /// Creates a new `RequestInfo` instance.
+    #[cfg(all(scylla_unstable, feature = "unstable-python-rs"))]
+    pub fn new(
+        error: &'a RequestAttemptError,
+        is_idempotent: bool,
+        consistency: Consistency,
+    ) -> Self {
+        Self {
+            error,
+            is_idempotent,
+            consistency,
+        }
+    }
+}
+
 /// Returned by implementations of RetryPolicy. Instructs the driver on what
 /// to do about the request after it failed.
 #[derive(Clone, Debug, PartialEq, Eq)]
