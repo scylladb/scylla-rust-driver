bdde9e3add9d2ac0f108d094b36fc0182e4c5697
Karol Baryła <karol.baryla@scylladb.com>
2026-06-02 16:28:11 +0200

    DefaultPolicy: Adjust warning message
    Mentioning DC preference should be more understandable to users than
    mentioning field name that doesn't exist, or `Some` that is not visible
    to users.

--- scylla/src/policies/load_balancing/default.rs
+++ scylla/src/policies/load_balancing/default.rs
@@ -161,7 +161,7 @@ impl LoadBalancingPolicy for DefaultPolicy {
         {
             warn!(
                 "\
-Combining SimpleStrategy with preferred_datacenter set to Some and disabled datacenter failover may lead to empty query plans for some tokens.\
+Combining SimpleStrategy with DC preference and disabled datacenter failover may lead to empty query plans for some tokens.\
 It is better to give up using one of them: either operate in a keyspace with NetworkTopologyStrategy, which explicitly states\
 how many replicas there are in each datacenter (you probably want at least 1 to avoid empty plans while preferring that datacenter), \
 or refrain from preferring datacenters (which may ban all other datacenters, if datacenter failover happens to be not possible)."
