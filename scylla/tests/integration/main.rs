f52ae1204358f9191667d0ca9f2d672685548a51
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2026-08-10 09:09:23 +0200

    IT: increase recursion_limit to fix clippy error
    The following errors keeps breaking the CI:
    
    error: queries overflow the depth limit!
      |
      = help: consider increasing the recursion limit by adding a `#![recursion_limit = "256"]` attribute to your crate (`integration`)
      = note: query depth increased by 130 when computing layout of `{async block@scylla/tests/integration/ccm/authenticate.rs:39:1: 39:15}`
    
    Let's try implementing the advice.

--- scylla/tests/integration/main.rs
+++ scylla/tests/integration/main.rs
@@ -1,3 +1,9 @@
+// This hopefully fixes the following error that occurs in CI:
+// error: queries overflow the depth limit!
+//   |
+//   = help: consider increasing the recursion limit by adding a `#![recursion_limit = "256"]` attribute to your crate (`integration`)
+//   = note: query depth increased by 130 when computing layout of `{async block@scylla/tests/integration/ccm/authenticate.rs:39:1: 39:15}`
+#![recursion_limit = "256"]
 // Rigorous documentation is not necessary for integration tests.
 #![allow(missing_docs)]
 
